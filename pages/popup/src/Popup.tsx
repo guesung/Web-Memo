7cdcc91e3301eff8a5a72f90e05c35a188440f10
guesung <gueit214@naver.com>
2024-08-12 12:27:10 +0900

    design: 기존 스타일 및 기능을 제거한다

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,27 +1,12 @@
-import '@src/Popup.css';
 import { useStorageSuspense, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
-import { ComponentPropsWithoutRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Popup = () => {
   const theme = useStorageSuspense(exampleThemeStorage);
   const isLight = theme === 'light';
-  const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
   const [pageText, setPageText] = useState('');
-
-  const injectContentScript = async () => {
-    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    await chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      /**
-       * If you are using Firefox, you should use a relative path. :(
-       * @example
-       * files: ['../content-runtime/index.iife.js'],
-       */
-      files: ['content-runtime/index.iife.js'],
-    });
-  };
+  const [summary, setSummary] = useState('');
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
@@ -41,49 +26,26 @@ const Popup = () => {
   }, []);
 
   const handlePopup = () => {
-    chrome.runtime.sendMessage({
-      type: 'summarize',
-      payload: {
-        pageText,
+    chrome.runtime.sendMessage(
+      {
+        type: 'summarize',
+        payload: {
+          pageText,
+        },
       },
-    });
+      response => {
+        setSummary(response.message);
+      },
+    );
   };
 
   return (
     <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
       <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>pages/popup/src/Popup.tsx</code>
-        </p>
-        <button
-          className={
-            'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-            (isLight ? 'bg-blue-200 text-black' : 'bg-gray-700 text-white')
-          }
-          onClick={injectContentScript}>
-          Click to inject Content Script
-        </button>
-        <ToggleButton>Toggle theme</ToggleButton>
-        <button onClick={handlePopup}>번역</button>
+        <button onClick={handlePopup}>Summarize this page !</button>
+        {summary && <p>{summary}</p>}
       </header>
     </div>
-  );
-};
-
-const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
-  const theme = useStorageSuspense(exampleThemeStorage);
-  return (
-    <button
-      className={
-        props.className +
-        ' ' +
-        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-        (theme === 'light' ? 'bg-white text-black shadow-black' : 'bg-black text-white')
-      }
-      onClick={exampleThemeStorage.toggle}>
-      {props.children}
-    </button>
   );
 };
 

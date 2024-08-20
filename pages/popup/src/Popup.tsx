e68b43fe9b87e40316052931ceae5c9dae341076
guesung <gueit214@naver.com>
2024-08-20 20:57:22 +0900

    feat: Markdown컴포넌트를 추가한다

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,10 +1,11 @@
 import { withErrorBoundary, withSuspense } from '@extension/shared';
 import { useEffect, useState } from 'react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { queryPageSummaryFromBackground, queryPageTextFromTab } from './query';
 
 const Popup = () => {
   const [summary, setSummary] = useState('');
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -14,13 +15,14 @@ const Popup = () => {
       console.log(pageSummary);
       setSummary(pageSummary);
     })();
-    setLoading(false);
   }, []);
 
   if (!summary) return <span className="loading loading-ring loading-md" />;
   return (
-    <header className={`bg-base-300 block ${summary && 'mockup-window'}`}>
-      <p className="px-8 py-4 bg-base-200 min-w-[800px] break-words">{summary}</p>
+    <header className="px-4 ">
+      <Markdown remarkPlugins={[remarkGfm]} className="markdown-content w-[600px]">
+        {summary}
+      </Markdown>
     </header>
   );
 };

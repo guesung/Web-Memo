d7eb91d8a03e2c21fc367c180ccac62806c2bead
guesung <gueit214@naver.com>
2024-08-26 12:37:46 +0900

    feat: package 스크립트를 추가한다

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -25,7 +25,6 @@ const manifest = deepmerge(
     name: 'Page Summary',
     default_locale: 'en',
     version: packageJson.version,
-    description: 'Page Summary는 웹 페이지의 내용을 미리 요약해주어, 글을 읽기 더 쉽게 만들어주는 프로그램입니다.',
     host_permissions: ['<all_urls>'],
     options_page: 'options/index.html',
     background: {

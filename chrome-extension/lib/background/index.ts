aad8a1f20d154eb76ddf65affc5c7a409c5b7568
guesung <gueit214@naver.com>
2024-08-30 15:18:00 +0900

    feat: chrome.sidePanel.setOptions을 설치 시 진행한다

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -3,15 +3,18 @@ import { PROMPT } from '@root/constants';
 import { openai } from '@root/utils/openai';
 import 'webextension-polyfill';
 
-chrome.runtime.onMessage.addListener((bridgeResponse: BridgeRequest, sender, sendResponse) => {
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.sidePanel.setOptions({
+    path: 'side-panel/index.html',
+    enabled: true,
+  });
+});
+
+chrome.runtime.onMessage.addListener((bridgeResponse: BridgeRequest, sender) => {
   (async () => {
+    if (!sender.tab) return;
     if (bridgeResponse.type === BRIDGE_TYPE_OPEN_SIDE_PANEL) {
-      await chrome.sidePanel.open({ tabId: sender.tab.id });
-      await chrome.sidePanel.setOptions({
-        tabId: sender.tab.id,
-        path: 'side-panel/index.html',
-        enabled: true,
-      });
+      await chrome.sidePanel.open({ windowId: sender.tab.windowId });
     }
   })();
 });
@@ -24,6 +27,7 @@ chrome.runtime.onConnect.addListener(async port => {
       messages: [{ role: 'user', content: PROMPT + pageContent }],
       stream: true,
     });
+
     for await (const chunk of stream) {
       const message = chunk.choices[0]?.delta?.content;
       if (message === undefined) port.disconnect();

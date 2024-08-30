2ab7bd1fd3ef986564e0287fd696b89bb1c60a51
guesung <gueit214@naver.com>
2024-08-30 15:18:15 +0900

    docs: 르폼프트에서 `video`에서 `website`로 변경한다

--- chrome-extension/constants/prompt.ts
+++ chrome-extension/constants/prompt.ts
@@ -1,9 +1,9 @@
 export const PROMPT = `
 Language: Please write in 한국어 language! Please write in 한국어 language!Please write in 한국어 language!
 
-Instructions:Please answer within 200-800 characters. Your task is to summarize the video content using the provided subtitles. Your output should use the following template:
+Instructions:Please answer within 200-800 characters. Your task is to summarize the website content using the provided subtitles. Your output should use the following template:
 #### Summary
-{Summarize the video content in one sentence, ensuring it's no more than 30 words.}
+{Summarize the website content in one sentence, ensuring it's no more than 30 words.}
 #### Highlights
 {Provide 5 highlights, starting with a summarizing phrase. }
 

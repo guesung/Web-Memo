e13025a85bb952faeb9b9ab05be915298b01f403
guesung <gueit214@naver.com>
2024-09-01 16:41:00 +0900

    feat: toss/overlay-kit를 이용하여 toast를 보여준다

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,3 +1,5 @@
+import { OverlayProvider, overlay } from 'overlay-kit';
+
 import { BRIDGE_TYPE_SUMMARY, queryPageTextFromTab, Summary, withErrorBoundary, withSuspense } from '@extension/shared';
 import { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
@@ -38,7 +40,16 @@ const SidePanel = () => {
       url: tab.url,
       date: new Date().toISOString(),
     };
-    await chrome.storage.sync.set({ summaryList: [...prevSummaryList, newSummary] });
+    // await chrome.storage.sync.set({ summaryList: [...prevSummaryList, newSummary] });
+    overlay.open(({ open, close }) => {
+      return (
+        <div className="toast toast-end">
+          <div className="alert alert-success">
+            <span>Message sent successfully.</span>
+          </div>
+        </div>
+      );
+    });
   };
 
   useEffect(() => {
@@ -48,54 +59,56 @@ const SidePanel = () => {
   }, []);
 
   return (
-    <main className="prose prose-sm">
-      <header className="navbar flex justify-center">
-        {isSummaryLoading ? (
-          <span className="loading loading-bars loading-sm" />
-        ) : (
-          <div className="flex gap-4">
-            <button onClick={startSummary}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width={24}>
-                <circle cx="50" cy="50" r="45" fill="currentColor" />
-                <polygon points="40,30 70,50 40,70" fill="white" />
-              </svg>
-            </button>
-            <button onClick={startSave}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24}>
-                <path
-                  d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                />
-                <polyline
-                  points="17 21 17 13 7 13 7 21"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                />
-                <polyline
-                  points="7 3 7 8 15 8"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                />
-              </svg>
-            </button>
-          </div>
-        )}
-      </header>
+    <OverlayProvider>
+      <main className="prose prose-sm">
+        <header className="navbar flex justify-center">
+          {isSummaryLoading ? (
+            <span className="loading loading-bars loading-sm" />
+          ) : (
+            <div className="flex gap-4">
+              <button onClick={startSummary}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width={24}>
+                  <circle cx="50" cy="50" r="45" fill="currentColor" />
+                  <polygon points="40,30 70,50 40,70" fill="white" />
+                </svg>
+              </button>
+              <button onClick={startSave}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24}>
+                  <path
+                    d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  />
+                  <polyline
+                    points="17 21 17 13 7 13 7 21"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  />
+                  <polyline
+                    points="7 3 7 8 15 8"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </button>
+            </div>
+          )}
+        </header>
 
-      <Markdown remarkPlugins={[remarkGfm]} className="markdown px-4">
-        {summary}
-      </Markdown>
-    </main>
+        <Markdown remarkPlugins={[remarkGfm]} className="markdown px-4">
+          {summary}
+        </Markdown>
+      </main>
+    </OverlayProvider>
   );
 };
 

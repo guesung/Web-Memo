1d6b10eb29042ef9993c9b326843b56d32dc11eb
guesung <gueit214@naver.com>
2024-09-01 15:55:24 +0900

    feat: content scritp에서 storage 데이터를 불러와 보여준다

--- pages/content-ui/src/components/PageSummaryList.tsx
+++ pages/content-ui/src/components/PageSummaryList.tsx
@@ -2,14 +2,22 @@ import { Summary } from '@extension/shared';
 import { useEffect, useState } from 'react';
 
 export default function PageSummaryList() {
-  const [summaryList, setSummaryList] = useState<Summary>([]);
+  const [summaryList, setSummaryList] = useState<Summary[]>([]);
   useEffect(() => {
     (async () => {
-      const response = await chrome.storage.sync.get('summaryList');
-      console.log(response);
-      // setSummaryList
+      const { summaryList } = await chrome.storage.sync.get('summaryList');
+      setSummaryList(summaryList);
     })();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {summaryList.map(it => (
+        <div key={it.date}>
+          <h2>{it.title}</h2>
+          <p>{it.summary}</p>
+        </div>
+      ))}
+    </div>
+  );
 }

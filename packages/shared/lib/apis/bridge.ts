bcd752ed92d9d39aa71ebd1cb5154cd0a944ecbb
guesung <gueit214@naver.com>
2024-09-01 15:18:43 +0900

    refactor: queryPageTextFromTab을 async-await문으로 리팩토링

--- packages/shared/lib/apis/bridge.ts
+++ packages/shared/lib/apis/bridge.ts
@@ -2,27 +2,17 @@ import { BRIDGE_TYPE_PAGE_CONTENT } from '../constants';
 import { BridgeRequest, BridgeResponse } from '../types';
 
 // Tab으로부터 페이지 텍스트를 가져온다.
-export const queryPageTextFromTab = (): Promise<string> =>
-  new Promise(resolve => {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-      const [activeTab] = tabs;
-      if (activeTab.id) {
-        chrome.tabs.sendMessage<BridgeRequest, BridgeResponse>(
-          activeTab.id,
-          { type: BRIDGE_TYPE_PAGE_CONTENT },
-          response => {
-            if (chrome.runtime.lastError) {
-              console.error(chrome.runtime.lastError);
-              return;
-            }
-            if (response && response.message) {
-              resolve(response.message);
-            }
-          },
-        );
-      }
+export const queryPageTextFromTab = async () => {
+  try {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage<BridgeRequest, BridgeResponse>(tab.id, {
+      type: BRIDGE_TYPE_PAGE_CONTENT,
     });
-  });
+    return response?.message;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
 
 export const responsePageTextFromTab = () => {
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {

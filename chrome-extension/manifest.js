ec9b93bd62133d47af83fa4b841d087cfa5d81dd
guesung <gueit214@naver.com>
2024-09-02 22:34:47 +0900

    feat: 권한에 storage, tabs, contextMenus를 추가한다

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -25,7 +25,7 @@ const manifest = deepmerge(
     name: 'Page Summary',
     default_locale: 'en',
     version: packageJson.version,
-    permissions: ['sidePanel'],
+    permissions: ['sidePanel', 'storage', 'tabs', 'contextMenus'],
     host_permissions: ['<all_urls>'],
     description: 'Page Summary is a browser extension that helps you summarize web pages.',
     options_page: 'options/index.html',

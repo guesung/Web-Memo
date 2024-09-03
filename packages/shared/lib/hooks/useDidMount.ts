8909cf9b57f6ee9b4b4625e918a6cc587d0f5950
guesung <gueit214@naver.com>
2024-09-03 12:46:11 +0900

    feat: 마운트 시 한 번만 실행하는 `useDidMount`훅을 구현한다

---
+++ packages/shared/lib/hooks/useDidMount.ts
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+
+export default function useDidMount(callbackFn: () => void) {
+  const mountRef = useRef(false);
+
+  useEffect(() => {
+    if (mountRef.current) return;
+    callbackFn();
+    mountRef.current = true;
+  }, []);
+
+  return null;
+}

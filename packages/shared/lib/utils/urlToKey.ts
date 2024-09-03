0e9f4d1b2d12d122ea00288afa2cd47e8d56559b
guesung <gueit214@naver.com>
2024-09-03 12:55:50 +0900

    fix: urlToKey에서, url이 빈 문자열일 경우 리턴한다

--- packages/shared/lib/utils/urlToKey.ts
+++ packages/shared/lib/utils/urlToKey.ts
@@ -1,4 +1,6 @@
 export const urlToKey = (url: string) => {
+  if (!url) return '';
+
   const urlWithoutFragment = url.split('#').at(0);
 
   if (!urlWithoutFragment) throw new Error('Invalid URL');

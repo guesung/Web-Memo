799dbcdb158ceca6a3b324925527106799718f93
guesung <gueit214@naver.com>
2024-09-03 12:46:28 +0900

    feat: 데이터 패칭을 한 번만 수행하는 `useFetch`훅을 구현한다

--- packages/shared/lib/hooks/index.ts
+++ packages/shared/lib/hooks/index.ts
@@ -1,1 +1,4 @@
 export * from './useStorage';
+export { default as useFetch } from './useFetch';
+export { default as useUrl } from './useUrl';
+export { default as useDidMount } from './useDidMount';
---
+++ packages/shared/lib/hooks/useFetch.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import useDidMount from './useDidMount';
+
+interface UseFetchProps<TData> {
+  fetchFn: () => Promise<TData>;
+  defaultValue: TData;
+}
+
+export default function useFetch<TData>({ fetchFn, defaultValue }: UseFetchProps<TData>) {
+  const [data, setData] = useState<TData>(defaultValue);
+
+  useDidMount(async () => {
+    const data = await fetchFn();
+    setData(data);
+  });
+
+  return { data };
+}

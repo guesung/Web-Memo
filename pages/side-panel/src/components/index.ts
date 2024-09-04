904a2be264c1c38ac996237a46a9b20b77c9974d
guesung <gueit214@naver.com>
2024-09-04 19:33:29 +0900

    refactor: 사용하지 않는 컴포넌트를 제거한다

--- pages/side-panel/src/components/index.ts
+++ pages/side-panel/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Loading } from './Loading';
-export { default as Header } from './Header';
 export { default as Summary } from './Summary';
 export { default as Memo } from './Memo';
--- pages/side-panel/src/icons/index.ts
+++ pages/side-panel/src/icons/index.ts
@@ -1,4 +1,2 @@
-export { default as Save } from './Save';
-export { default as Start } from './Start';
 export { default as Refresh } from './Refresh';
 export { default as Option } from './Option';

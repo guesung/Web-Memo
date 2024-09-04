cbcd32a2b16089f73b82352e7929c74c22316b2f
guesung <gueit214@naver.com>
2024-09-04 19:30:16 +0900

    feat: 로딩과 클릭에 대한 이벤트 처리를 Refresh내부에서 처리한다

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,7 +1,7 @@
 import { OverlayProvider } from 'overlay-kit';
 
 import { withErrorBoundary, withSuspense } from '@extension/shared';
-import { Loading, Memo, Summary } from './components';
+import { Memo, Summary } from './components';
 import { useGetSummary } from './hooks';
 import { Option, Refresh } from './icons';
 
@@ -15,13 +15,7 @@ const SidePanel = () => {
     <OverlayProvider>
       <main className="prose prose-sm flex flex-col h-lvh px-4 relative">
         <div className="absolute right-4 top-4 flex gap-1 items-center">
-          {isSummaryLoading ? (
-            <Loading />
-          ) : (
-            <button onClick={startSummary}>
-              <Refresh width="20px" height="20px" />
-            </button>
-          )}
+          <Refresh width="20px" height="20px" isLoading={isSummaryLoading} onRefresh={startSummary} />
           <button onClick={handleOptionClick}>
             <Option width="24px" height="24px" />
           </button>

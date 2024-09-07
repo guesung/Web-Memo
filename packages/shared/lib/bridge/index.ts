77dd03e912abe6224aec9b56ff840793ffa802e5
guesung <gueit214@naver.com>
2024-09-07 14:21:57 +0900

    refactor: bridge폴더에서 lib 내 bridge폴더로 옮긴다

--- packages/shared/lib/bridge/index.ts
+++ packages/shared/lib/bridge/index.ts
@@ -1,6 +1,5 @@
 export * from './pageContent';
 export * from './openSidePanel';
 export * from './type';
-export * from './shared';
 export * from './getSummary';
 export * from './updateSidePanel';

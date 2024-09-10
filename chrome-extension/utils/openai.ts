6035219fbfa281539d83a15cfb8d5527c08991fd
guesung <gueit214@naver.com>
2024-09-10 14:05:50 +0900

    feat: chrome-extension 프로젝트에 환경 변수를 추가한다

--- chrome-extension/utils/openai.ts
+++ chrome-extension/utils/openai.ts
@@ -1,6 +1,5 @@
-import { OPENAI_API_KEY } from '@root/constants';
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });

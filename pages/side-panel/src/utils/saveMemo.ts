ba400a14562a5f72f676b53eac25468341933341
guesung <gueit214@naver.com>
2024-09-10 11:31:42 +0900

    refactor: `getMemoData`로 모듈화를 한다

--- pages/side-panel/src/utils/saveMemo.ts
+++ pages/side-panel/src/utils/saveMemo.ts
@@ -1,32 +1,30 @@
 import { MemoStorage, Tab, urlToKey } from '@extension/shared';
 
-export const saveMemoStorage = async (memo: string) => {
+const getMemoData = async (memo: string) => {
   const tab = await Tab.get();
 
   if (!tab.url || !tab.title) throw new Error('Save Failed: Invalid URL');
 
-  const urlKey = urlToKey(tab.url);
-
-  await MemoStorage.set(urlKey, {
+  return {
     title: tab.title,
     memo,
     date: new Date().toISOString(),
     url: tab.url,
-  });
+  };
+};
+
+export const saveMemoStorage = async (memo: string) => {
+  const memoData = await getMemoData(memo);
+  const urlKey = urlToKey(memoData.url);
+
+  await MemoStorage.set(urlKey, memoData);
 };
 
 export const saveMemoNotion = async (memo: string) => {
-  const tab = await Tab.get();
+  const memoData = await getMemoData(memo);
 
-  if (!tab.url || !tab.title) throw new Error('Save Failed: Invalid URL');
-
-  await fetch(import.meta.env.VITE_MAKE_WEBHOOK_NOTION_API, {
+  const response = await fetch(import.meta.env.VITE_MAKE_WEBHOOK_NOTION_API, {
     method: 'POST',
-    body: JSON.stringify({
-      title: tab.title,
-      memo,
-      date: new Date().toISOString(),
-      url: tab.url,
-    }),
+    body: JSON.stringify(memoData),
   });
 };

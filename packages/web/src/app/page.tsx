2b9eba4a0a09fb68db3dfbf697e243bfe5c35aab
guesung <gueit214@naver.com>
2024-09-15 21:34:20 +0900

    feat: web의 틀을 잡는다

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main></main>;
+import { Footer, Header, Introduce } from '@src/components';
+
+export default function Page() {
+  return (
+    <main>
+      <Header />
+      <Introduce />
+      <Footer />
+    </main>
+  );
 }
---
+++ packages/web/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from './Header';
+export { default as Introduce } from './Introduce';
+export { default as Footer } from './Footer';

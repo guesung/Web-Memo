0ec6f835c3f9cde75de1d04d8fe27da9a86c4d43
guesung <gueit214@naver.com>
2024-09-16 16:04:49 +0900

    refactor: 사용하지 않는 코드를 제거한다

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { Footer, Header, Introduce } from '@src/components';
-import { createClient } from '@supabase/supabase-js';
-const a = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 export default function Page() {
   return (

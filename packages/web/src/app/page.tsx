3149aa8cac6164fbd9a333e6e19067d6fd387b19
guesung <gueit214@naver.com>
2024-09-16 14:30:14 +0900

    feat: supabase client를 하나로 통일한다

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,4 +1,7 @@
+'use client';
 import { Footer, Header, Introduce } from '@src/components';
+import { createClient } from '@supabase/supabase-js';
+const a = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 export default function Page() {
   return (

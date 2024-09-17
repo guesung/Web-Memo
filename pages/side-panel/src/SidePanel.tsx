efc56a5ca2bff5e60609dcbee40217b94bea1da2
guesung <gueit214@naver.com>
2024-09-17 15:56:55 +0900

    refactor: withSuspense를 제거한다

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,10 +1,9 @@
 import { OverlayProvider } from 'overlay-kit';
 
-import { withSuspense } from '@extension/shared';
 import { ErrorBoundary } from '@extension/ui';
 import { Header, Memo, Summary, SummaryProvider } from './components';
 
-const SidePanel = () => {
+export default function SidePanel() {
   return (
     <OverlayProvider>
       <main className="prose prose-sm flex flex-col h-lvh px-4 relative">
@@ -24,6 +23,4 @@ const SidePanel = () => {
       </main>
     </OverlayProvider>
   );
-};
-
-export default withSuspense(SidePanel, <div> Loading ... </div>);
+}

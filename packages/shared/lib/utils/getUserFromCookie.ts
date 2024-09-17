fc065a5d319d9f1b6bcf48fe5f94e3613d47ec92
guesung <gueit214@naver.com>
2024-09-17 10:28:54 +0900

    feat: 환경변수를 config파일에서 관리하고, 기본값을 넣는다

--- packages/shared/lib/utils/getUserFromCookie.ts
+++ packages/shared/lib/utils/getUserFromCookie.ts
@@ -1,10 +1,10 @@
 import type { UserResponse } from '@supabase/supabase-js';
-import { authToken } from '../constants';
+import { authToken, WEB_URL } from '../constants';
 
 export const getUserFromCookie = async () => {
   const cookie = await chrome.cookies.get({
     name: authToken,
-    url: process.env.WEB_URL!,
+    url: WEB_URL,
   });
   if (!cookie) return;
 

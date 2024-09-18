2dc9e8aecce57dcfb589d61a39f72afdf1b2d0af
guesung <gueit214@naver.com>
2024-09-18 15:17:46 +0900

    refactor: config상수를 `getSafeConfig`로 래핑한다

--- pages/side-panel/src/constants/config.ts
+++ pages/side-panel/src/constants/config.ts
@@ -1,2 +1,4 @@
-export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-export const WEBHOOK_NOTION_API = import.meta.env.VITE_MAKE_WEBHOOK_NOTION_API;
+import { getSafeConfig } from '@extension/shared/lib/utils';
+
+export const SENTRY_DSN = getSafeConfig('SENTRY_DSN', import.meta.env.VITE_SENTRY_DSN);
+export const WEBHOOK_NOTION_API = getSafeConfig('WEBHOOK_NOTION_API', import.meta.env.VITE_MAKE_WEBHOOK_NOTION_API);

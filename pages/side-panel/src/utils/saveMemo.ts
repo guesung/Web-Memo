3348c8224d0c3200109f645f250e2a3363d95b24
guesung <gueit214@naver.com>
2024-09-19 15:08:58 +0900

    feat: supabse에 메모를 저장하는 `saveMemoSupabase`를 구현한다

--- pages/side-panel/src/utils/saveMemo.ts
+++ pages/side-panel/src/utils/saveMemo.ts
@@ -1,7 +1,7 @@
-import { MemoStorage, Tab, urlToKey } from '@extension/shared';
+import { updateMemo, MemoStorage, Tab, urlToKey, getMemo, MemoType, insertMemo } from '@extension/shared';
 import { WEBHOOK_NOTION_API } from '@src/constants';
 
-const getMemoData = async (memo: string) => {
+const getMemoMetaData = async (memo: string): Promise<MemoType> => {
   const tab = await Tab.get();
 
   if (!tab.url || !tab.title) throw new Error('Save Failed: Invalid URL');
@@ -15,18 +15,29 @@ const getMemoData = async (memo: string) => {
 };
 
 export const saveMemoStorage = async (memo: string) => {
-  const memoData = await getMemoData(memo);
+  const memoData = await getMemoMetaData(memo);
   const urlKey = urlToKey(memoData.url);
 
   await MemoStorage.set(urlKey, memoData);
 };
 
 export const saveMemoNotion = async (memo: string) => {
-  const memoData = await getMemoData(memo);
+  const memoData = await getMemoMetaData(memo);
 
   const response = await fetch(WEBHOOK_NOTION_API, {
     method: 'POST',
     body: JSON.stringify(memoData),
   });
   return response;
+};
+
+export const saveMemoSupabase = async (memo: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { date, ...memoData } = await getMemoMetaData(memo);
+  const { data: memoList } = await getMemo();
+
+  const urlMatchMemo = memoList?.find(memo => memo.url === memoData.url);
+
+  if (urlMatchMemo === undefined) await insertMemo(memoData);
+  else await updateMemo(memoData);
 };

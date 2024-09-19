1ed9b987a7f99b64d5221d572ca026e333a98e39
guesung <gueit214@naver.com>
2024-09-19 19:36:03 +0900

    chore: supabase 타입을 기존 함수에 설정한다

--- packages/shared/src/utils/extension/supabase.ts
+++ packages/shared/src/utils/extension/supabase.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'src/types';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../../constants';
 import { getSession } from './getSession';
-import { MemoType } from 'src/types';
 
 export const getSupabaseClient = async () => {
   const user = await getSession();
   if (!user) throw new Error('없는 사용자입니다.');
 
-  const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  const supabaseClient = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
     db: { schema: 'memo' },
     global: {
       headers: {
@@ -23,19 +23,17 @@ export const getMemo = async () => {
   return await supabaseClient.from('memo').select('*');
 };
 
-interface MemoSupabaseRequest extends Omit<MemoType, 'date'> {}
-
-export const insertMemo = async (memoRequest: MemoSupabaseRequest | MemoSupabaseRequest[]) => {
+export const insertMemo = async (memoRequest: Database['memo']['Tables']['memo']['Insert']) => {
   const supabaseClient = await getSupabaseClient();
   return await supabaseClient.from('memo').insert(memoRequest).select();
 };
 
-export const updateMemo = async (memoRequest: MemoSupabaseRequest) => {
+export const updateMemo = async (memoRequest: Database['memo']['Tables']['memo']['Update']) => {
   const supabaseClient = await getSupabaseClient();
   return await supabaseClient.from('memo').update(memoRequest).eq('url', memoRequest).select();
 };
 
-export const upsertMemo = async (memoRequest: MemoSupabaseRequest | MemoSupabaseRequest[]) => {
+export const upsertMemo = async (memoRequest: Database['memo']['Tables']['memo']['Insert']) => {
   const supabaseClient = await getSupabaseClient();
   return await supabaseClient.from('memo').upsert(memoRequest).select();
 };
--- packages/web/src/utils/supabase.ts
+++ packages/web/src/utils/supabase.ts
@@ -1,9 +1,9 @@
-import { isServer } from '@extension/shared';
+import { Database, isServer } from '@extension/shared';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@src/constants';
 import { createClient } from '@supabase/supabase-js';
 
 const getSupabaseClient = () => {
-  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  return createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
       storage: {
         getItem: key => {

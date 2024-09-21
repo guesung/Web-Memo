e4bf9e4dced001b87a9a01d3b65b68c95eda2dbb
guesung <gueit214@naver.com>
2024-09-21 15:51:27 +0900

    feat: web/memo 페이지에 이동시, 페이지를 보여준다

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -1,40 +1,27 @@
 import { createRoot } from 'react-dom/client';
-import App from '@src/app';
 // eslint-disable-next-line
 // @ts-ignore
 import tailwindcssOutput from '@src/tailwind-output.css?inline';
 import { responsePageContent } from '@extension/shared';
+import { MemoTable } from '@extension/ui/lib/components';
 
-const root = document.createElement('div');
-root.id = 'page-summary';
+const memoRoot = document.getElementById('memo');
 
-document.body.append(root);
+if (memoRoot) {
+  document.body.append(memoRoot);
+  const rootIntoShadow = document.createElement('div');
+  rootIntoShadow.id = 'shadow-memo-root';
 
-const rootIntoShadow = document.createElement('div');
-rootIntoShadow.id = 'shadow-root';
+  const shadowRoot = memoRoot.attachShadow({ mode: 'open' });
 
-const shadowRoot = root.attachShadow({ mode: 'open' });
+  const globalStyleSheet = new CSSStyleSheet();
+  globalStyleSheet.replaceSync(tailwindcssOutput);
+  shadowRoot.adoptedStyleSheets = [globalStyleSheet];
 
-/** Inject styles into shadow dom */
-const globalStyleSheet = new CSSStyleSheet();
-globalStyleSheet.replaceSync(tailwindcssOutput);
-shadowRoot.adoptedStyleSheets = [globalStyleSheet];
-/**
- * In the firefox environment, the adoptedStyleSheets bug may prevent style from being applied properly.
- *
- * @url https://bugzilla.mozilla.org/show_bug.cgi?id=1770592
- * @url https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/pull/174
- *
- * Please refer to the links above and try the following code if you encounter the issue.
- *
+  responsePageContent();
 
- * const styleElement = document.createElement('style');
- * styleElement.innerHTML = tailwindcssOutput;
- * shadowRoot.appendChild(styleElement);
- * ```
- */
+  shadowRoot.appendChild(rootIntoShadow);
 
-responsePageContent();
-
-shadowRoot.appendChild(rootIntoShadow);
-createRoot(rootIntoShadow).render(<App />);
+  console.log(1);
+  createRoot(rootIntoShadow).render(<MemoTable />);
+}

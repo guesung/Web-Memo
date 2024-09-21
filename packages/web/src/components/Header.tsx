475f3bb8e11f8999cb33fe122983fcb44f432ab9
guesung <gueit214@naver.com>
2024-09-21 21:23:02 +0900

    feat: header에 메모 링크를 추가한다

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -5,9 +5,14 @@ export default function Header() {
   return (
     <header className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Icon</a>
-        <Link className="btn btn-ghost text-xl" href="login">
+        <Link href="/" className="link-hover btn btn-ghost text-xl">
+          Icon
+        </Link>
+        <Link className="link-hover btn btn-ghost text-xl" href="login">
           로그인
+        </Link>
+        <Link className="link-hover btn btn-ghost text-xl" href="memo">
+          나의 메모
         </Link>
       </div>
     </header>

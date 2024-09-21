76e8ca215dfefb8ecf88f5c3f3ad99d118659ba9
guesung <gueit214@naver.com>
2024-09-21 20:54:15 +0900

    Revert "refactor: `MemoTable`을 공용 ui컴포넌트로 옮긴다"
    This reverts commit 9c8131f9e9c05cb81dc5c7b11734332c0d5e7dba.

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -2,4 +2,3 @@ export { default as Toast } from './Toast';
 export { default as Loading } from './Loading';
 export { default as ErrorFallback } from './ErrorFallback';
 export { default as ErrorBoundary } from './ErrorBoundary';
-export { default as MemoTable } from './MemoTable';

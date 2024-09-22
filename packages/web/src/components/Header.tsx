803037664c6efb93f6fb6461bb68686c253b5ab7
guesung <gueit214@naver.com>
2024-09-22 11:04:20 +0900

    feat: 준비 중인 페이지 및 컴포넌트를 사용자에게 알린다

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -1,19 +1,23 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface HeaderProps {}
 export default function Header() {
   return (
-    <header className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link href="/" className="link-hover btn btn-ghost text-xl">
-          Icon
+    <header className="navbar bg-base-100 flex-1 w-full">
+      <div className="navbar-start">
+        <Link href="/" className="btn btn-ghost text-xl">
+          <Image src="/images/pngs/icon.png" width={24} height={24} alt="icon" />
+          <span>웹 메모</span>
         </Link>
-        <Link className="link-hover btn btn-ghost text-xl" href="login">
-          로그인
-        </Link>
-        <Link className="link-hover btn btn-ghost text-xl" href="memo">
+      </div>
+      <div className="flex navbar-end">
+        <Link className="btn btn-ghost text-xl" href="memo">
           나의 메모
         </Link>
+        {/* <Link className="btn btn-ghost text-xl" href="login">
+          로그인
+        </Link> */}
       </div>
     </header>
   );

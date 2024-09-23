821365bb106550cf8593ac3a3ea64cb07fb73a7b
guesung <gueit214@naver.com>
2024-09-23 11:58:15 +0900

    refactor: `attachShadowTree`로 content-ui의 확장성을 넓힌다

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -2,38 +2,50 @@ import { createRoot } from 'react-dom/client';
 // eslint-disable-next-line
 // @ts-ignore
 import tailwindcssOutput from '@src/tailwind-output.css?inline';
-import { responseObserverMemoPage, responsePageContent, WEB_URL } from '@extension/shared';
+import {
+  MEMO_TABLE_ID,
+  MEMO_TABLE_WRAPPER_ID,
+  responseObserverMemoPage,
+  responsePageContent,
+  WEB_URL,
+} from '@extension/shared';
 import { MemoTable } from './components';
 
 interface AttachShadowTree {
-  shadowHost: HTMLElement;
   shadowTree: JSX.Element;
+  shadowHostId: string;
 }
 
-const attachShadowTree = ({ shadowHost, shadowTree }: AttachShadowTree) => {
-  const shadowRoot = shadowHost.attachShadow({ mode: 'open' });
+const attachShadowTree = ({ shadowTree, shadowHostId }: AttachShadowTree) => {
+  const shadowHostParent = document.createElement('div');
+  shadowHostParent.id = shadowHostId;
+  document.body.appendChild(shadowHostParent);
+
+  const shadowRoot = shadowHostParent.attachShadow({ mode: 'open' });
 
   const globalStyleSheet = new CSSStyleSheet();
   globalStyleSheet.replaceSync(tailwindcssOutput);
   shadowRoot.adoptedStyleSheets = [globalStyleSheet];
 
   createRoot(shadowRoot).render(shadowTree);
+  return shadowRoot;
 };
 
 const initMemoList = () => {
   if (location.href !== `${WEB_URL}/memo`) return;
 
-  const memoRoot = document.getElementById('memo');
-  const memoTable = document.getElementById('memo-table');
+  const memoRoot = document.getElementById(MEMO_TABLE_WRAPPER_ID);
+  const memoTable = document.getElementById(MEMO_TABLE_ID);
 
   if (!memoRoot || memoTable) return;
 
   attachShadowTree({
-    shadowHost: memoRoot,
+    shadowHostId: MEMO_TABLE_ID,
     shadowTree: <MemoTable />,
   });
 };
 
-initMemoList();
-responsePageContent();
 responseObserverMemoPage(initMemoList);
+initMemoList();
+
+responsePageContent();

f2a40b037efda5cd68dcf683f6691befa638c665
guesung <gueit214@naver.com>
2024-09-24 13:44:17 +0900

    refactor: tsc를 `build.mjs`에서 수행한다

--- packages/shared/build.mjs
+++ packages/shared/build.mjs
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 import * as esbuild from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 dotenv.config();
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 const define = {};
 
@@ -16,7 +20,6 @@ const buildOptions = {
   entryPoints: ['./src/**/index.ts'],
   outdir: 'dist',
   tsconfig: './tsconfig.json',
-  bundle: true,
   target: 'es6',
   plugins: [nodeExternalsPlugin()],
   define,
@@ -25,3 +28,5 @@ const buildOptions = {
 await esbuild.build({ ...buildOptions, format: 'cjs', outExtension: { '.js': '.cjs' } });
 
 await esbuild.build({ ...buildOptions, format: 'esm', outExtension: { '.js': '.js' }, sourcemap: true });
+
+await execAsync('tsc --emitDeclarationOnly --outDir dist');

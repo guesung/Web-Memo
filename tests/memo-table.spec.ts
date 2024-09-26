1945d5d03c370ba246aae1b0fb9d5dfcac9c7af3
guesung <gueit214@naver.com>
2024-09-26 14:47:32 +0900

    feat: `test.decribe`로 메모 테이블 관련 테스트를 묶는다

--- tests/memo-table.spec.ts
+++ tests/memo-table.spec.ts
@@ -1,8 +1,16 @@
 import { expect, test } from './fixtures';
 
-test('Memo Tabe의 title을 체크한다', async ({ page }) => {
-  await page.goto('http://localhost:3000/memo');
-  await expect(page).toHaveTitle('Web Memo');
+test.describe('메모 테이블을 테스트한다', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/memo');
+  });
+  test('title을 체크한다', async ({ page }) => {
+    await expect(page).toHaveTitle('Web Memo');
+  });
+  test('비었을 때를 체크한다.', async ({ page }) => {
+    const emptyElem = page.getByText('No Memo');
+    expect(emptyElem).toBeVisible();
+  });
 });
 
 const COLUMN_NAME_LIST = ['번호', '제목', '날짜', '메모', '삭제'];

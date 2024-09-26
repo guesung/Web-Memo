d1c155d09408b30b50709c2186439d2125de55ea
guesung <gueit214@naver.com>
2024-09-26 12:56:39 +0900

    fix: setting-env의 오타를 수정한다

--- scripts/setting-env.sh
+++ scripts/setting-env.sh
@@ -2,9 +2,9 @@ echo OPENAI_API_KEY=${{ secrets.OPENAI_API_KEY }} >> packages/shared/.env
 echo SENTRY_AUTH_TOKEN=${{ secrets.SENTRY_AUTH_TOKEN }} >> packages/shared/.env
 echo SENTRY_DSN=${{ secrets.SENTRY_DSN }} >> packages/shared/.env
 echo SUPABASE_URL=${{ secrets.SUPABASE_URL }} >> packages/shared/.env
-echo SUPABASE_ANON_KEY=${{ secrets.SUPABASE_ANON_KEY }}  >> packages/shared/.env
-echo NODE_ENV=${{ secrets.NODE_ENV }}  >> packages/shared/.env
-echo MAKE_WEBHOOK_NOTION_API=${{ secrets.MAKE_WEBHOOK_NOTION_API }}  >> packages/shared/.env
+echo SUPABASE_ANON_KEY=${{ secrets.SUPABASE_ANON_KEY }} >> packages/shared/.env
+echo NODE_ENV=${{ secrets.NODE_ENV }} >> packages/shared/.env
+echo MAKE_WEBHOOK_NOTION_API=${{ secrets.MAKE_WEBHOOK_NOTION_API }} >> packages/shared/.env
 echo WEB_URL=${{ secrets.WEB_URL }} >> packages/shared/.env
 
 echo NEXT_PUBLIC_SUPABASE_URL=${{ secrets.NEXT_PUBLIC_SUPABASE_URL }} >> packages/web/.env

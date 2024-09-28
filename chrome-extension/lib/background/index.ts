394d337a2656e1af164d0414378f157135423af9
guesung <gueit214@naver.com>
2024-09-28 14:04:30 +0900

    fix: `chrome.contextMenus.onClicked`을 background에서 실행한다

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -47,17 +47,19 @@ chrome.runtime.onInstalled.addListener(async () => {
     id: CONTEXT_MENU_ID_SHOW_GUIDE,
     contexts: ['action'],
   });
-  chrome.contextMenus.onClicked.addListener(async item => {
-    switch (item.menuItemId) {
-      case CONTEXT_MENU_ID_CHECK_MEMO:
-        await Tab.create({ url: `${WEB_URL}/memo` });
-        break;
-      case CONTEXT_MENU_ID_SHOW_GUIDE:
-        if (I18n.getUILanguage() === 'ko') Tab.create({ url: URL_GUIDE_KO });
-        else Tab.create({ url: URL_GUIDE_EN });
-        break;
-    }
-  });
+});
+
+// contextMenus 각 항목을 누를 때 해당하는 페이지로 이동한다.
+chrome.contextMenus.onClicked.addListener(async item => {
+  switch (item.menuItemId) {
+    case CONTEXT_MENU_ID_CHECK_MEMO:
+      await Tab.create({ url: `${WEB_URL}/memo` });
+      break;
+    case CONTEXT_MENU_ID_SHOW_GUIDE:
+      if (I18n.getUILanguage() === 'ko') Tab.create({ url: URL_GUIDE_KO });
+      else Tab.create({ url: URL_GUIDE_EN });
+      break;
+  }
 });
 
 chrome.runtime.setUninstallURL(URL_FORM);

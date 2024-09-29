ee408a43d3d1a49df27b99fba664abcb083386b0
guesung <gueit214@naver.com>
2024-09-29 13:46:32 +0900

    test

--- tests/memo-table.spec.ts
+++ tests/memo-table.spec.ts
@@ -8,18 +8,18 @@ test.describe('메모 테이블을 테스트한다', () => {
     await expect(page).toHaveTitle('Web Memo');
   });
   test('메모 테이블이 비었을 때를 체크한다.', async ({ page }) => {
-    const emptyElem = page.getByText('No Memo');
+    const emptyElem = page.getByText('There is no memo');
     expect(emptyElem).toBeVisible();
   });
 });
 
-const COLUMN_NAME_LIST = ['번호', '제목', '날짜', '메모', '삭제'];
+// const COLUMN_NAME_LIST = ['번호', '제목', '날짜', '메모', '삭제'];
 
-test('Memo Tabe의 columns를 체크한다', async ({ page }) => {
-  await page.goto('http://localhost:3000/memo');
+// test('Memo Tabe의 columns를 체크한다', async ({ page }) => {
+//   await page.goto('http://localhost:3000/memo');
 
-  for await (const columnName of COLUMN_NAME_LIST) {
-    const column = page.getByText(columnName, { exact: true });
-    expect(column).toBeVisible();
-  }
-});
+//   for await (const columnName of COLUMN_NAME_LIST) {
+//     const column = page.getByText(columnName, { exact: true });
+//     expect(column).toBeVisible();
+//   }
+// });

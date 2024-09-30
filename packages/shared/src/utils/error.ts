ce5846798e4a323f7d1651d696cfa8fb123c3656
guesung <gueit214@naver.com>
2024-09-30 14:33:32 +0900

    feat: Error객체를 다루는 유틸을 만든다

---
+++ packages/shared/src/utils/error.ts
@@ -0,0 +1,21 @@
+function isErrorWithMessage(error: unknown): error is Error {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as Record<string, unknown>).message === 'string'
+  );
+}
+
+export function toErrorWithMessage(maybeError: unknown): Error {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    return new Error(String(maybeError));
+  }
+}
+export function getErrorMeesage(error: unknown) {
+  return toErrorWithMessage(error).message;
+}

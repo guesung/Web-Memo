50882319ca1c56c32844eb91c36fa84fbb052a84
guesung <gueit214@naver.com>
2024-09-30 14:33:54 +0900

    feat: Error를 다루는 훅`useError`을 만든다

---
+++ packages/shared/src/hooks/useError.ts
@@ -0,0 +1,10 @@
+import { toErrorWithMessage } from '@src/utils';
+import { useState } from 'react';
+
+export default function useError() {
+  const [error, setError] = useState<Error | null>(null);
+
+  if (error) throw toErrorWithMessage(error);
+
+  return { error, setError } as const;
+}

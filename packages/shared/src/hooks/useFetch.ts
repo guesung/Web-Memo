f773e3ae2bdd974f2377d172baf33d4a9128e97a
guesung <gueit214@naver.com>
2024-09-30 12:33:49 +0900

    refactor: 불필요한 `AbortController`객체를 제거한다

--- packages/shared/src/hooks/useFetch.ts
+++ packages/shared/src/hooks/useFetch.ts
@@ -1,5 +1,6 @@
 import { I18n } from '@src/utils/extension';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
+import useDidMount from './useDidMount';
 
 interface UseFetchProps<TData> {
   fetchFn: () => Promise<TData>;
@@ -13,46 +14,20 @@ export default function useFetch<TData>({ fetchFn, defaultValue }: UseFetchProps
   const [status, setStatus] = useState<StatusType>('loading');
   const [error, setError] = useState<Error | null>(null);
 
-  const abortControllerRef = useRef<AbortController | null>(null);
-
   const fetch = useCallback(async () => {
-    if (abortControllerRef.current) {
-      // NOTE: 이미 동일한 요청이 있다면 취소한다
-      abortControllerRef.current.abort();
-    }
-
-    const abortController = new AbortController();
-    abortControllerRef.current = abortController;
     try {
       setStatus('loading');
       const data = await fetchFn();
-      if (abortController.signal.aborted) {
-        setStatus('aborted');
-        return;
-      }
 
       setData(data);
       setStatus('success');
     } catch (e) {
-      if (abortController.signal.aborted) {
-        setStatus('aborted');
-        return;
-      }
       setStatus('rejected');
       setError(e instanceof Error ? e : new Error(I18n.get('toast_error_common')));
-    } finally {
-      abortControllerRef.current = null;
     }
-  }, [fetchFn]);
+  }, [fetchFn, status]);
 
-  useEffect(() => {
-    fetch();
-
-    return () => {
-      if (abortControllerRef.current) abortControllerRef.current.abort();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  useDidMount(fetch);
 
   if (status === 'rejected' && error) throw error;
   return { data, error, refetch: fetch, status, isLoading: status === 'loading' };

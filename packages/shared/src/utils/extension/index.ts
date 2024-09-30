b357a09fb036ad9b088a46ffbce33c98c02894d3
guesung <gueit214@naver.com>
2024-09-30 15:52:08 +0900

    refactor: storage관련 유틸을 `storage`파일로 옮긴다

--- packages/shared/src/utils/extension/index.ts
+++ packages/shared/src/utils/extension/index.ts
@@ -3,5 +3,3 @@ export * from './module';
 
 export * from './supabase';
 export * from './storage';
-export * from './removeSession';
-export * from './storage';
--- packages/shared/src/utils/extension/removeSession.ts
+++
@@ -1,8 +0,0 @@
-import { SUPABASE_AUTH_TOKEN, WEB_URL } from '../../constants';
-
-export const removeSession = async () => {
-  await chrome.cookies.remove({
-    name: SUPABASE_AUTH_TOKEN,
-    url: WEB_URL,
-  });
-};

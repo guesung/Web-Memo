c1ac2147a9a00e62a63572e4c5917f18d92c360f
guesung <gueit214@naver.com>
2024-10-02 16:06:28 +0900

    feat: `YoutubeEmbed`를 구현한다

--- packages/web/src/components/index.ts
+++ packages/web/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Introduce } from './Introduce';
 export { default as Footer } from './Footer';
+export { default as YoutubeEmbed } from './YoutubeEmbed';

d95c9f57f36b730978dcc5bc2ead4bd4ddcb4c7d
guesung <gueit214@naver.com>
2024-10-02 22:05:55 +0900

    fix: 서버 에러로 인해, `'use client'`를 임시로 추가한다

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,14 +1,10 @@
-import type { Metadata } from 'next';
+// FIXME: supabase버그로 인해 'use client'를 추가한다
+'use client';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@src/components';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Web Memo',
-  description: 'Web Memo',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (

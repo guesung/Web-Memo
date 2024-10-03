dd3a82e38d612651f92ecef0ad39ac6092e917a1
guesung <gueit214@naver.com>
2024-10-03 22:32:20 +0900

    refactor: `useMemoMutation`을 타입 세이프하게 작성한다

--- pages/side-panel/src/hooks/useMemoMutation.ts
+++ pages/side-panel/src/hooks/useMemoMutation.ts
@@ -1,10 +1,11 @@
 import { formatUrl, insertMemo, updateMemo } from '@extension/shared/utils';
 import { getFormattedMemo, getSupabaseClient } from '@extension/shared/utils/extension';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query';
 import useTabQuery from './useTabQuery';
 import useMemoListQuery from './useMemoListQuery';
+import { MemoSupabaseResponse } from '@extension/shared/types';
 
-export default function useMemoMutation() {
+export default function useMemoMutation(useMutationProps: UseMutationOptions<MemoSupabaseResponse, Error, string>) {
   const queryClient = useQueryClient();
   const { data: memoList } = useMemoListQuery();
   const { data: tab } = useTabQuery();
@@ -13,17 +14,18 @@ export default function useMemoMutation() {
     const currentMemo = memoList?.data?.find(memo => memo.url === formatUrl(tab?.url));
     const supabaseClient = await getSupabaseClient();
 
-    if (currentMemo) await updateMemo(supabaseClient, { ...currentMemo, memo });
+    if (currentMemo) return await updateMemo(supabaseClient, { ...currentMemo, memo });
     else {
       const formattedMemo = await getFormattedMemo(memo);
-      await insertMemo(supabaseClient, formattedMemo);
+      return await insertMemo(supabaseClient, formattedMemo);
     }
   };
 
-  return useMutation({
+  return useMutation<MemoSupabaseResponse, Error, string>({
     mutationFn: saveMemo,
     onSettled: async () => {
       queryClient.invalidateQueries({ queryKey: ['memo-list'] });
     },
+    ...useMutationProps,
   });
 }

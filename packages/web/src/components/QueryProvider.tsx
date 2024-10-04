b00c3e824f46eb75a19ff4f9db941c179a592af8
guesung <gueit214@naver.com>
2024-10-04 19:23:19 +0900

    fix: 쿼리의 stale time을 설정한다

--- packages/web/src/components/QueryProvider.tsx
+++ packages/web/src/components/QueryProvider.tsx
@@ -1,7 +1,15 @@
 'use client';
 import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 export default function QueryProvider({ children }: PropsWithChildren) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }

a989fa9a55317b748f13529018ccc559579b1de9
guesung <gueit214@naver.com>
2024-10-04 20:58:26 +0900

    chore: queryClient를 상태값으로 선언한다

--- packages/web/src/components/QueryProvider.tsx
+++ packages/web/src/components/QueryProvider.tsx
@@ -1,15 +1,9 @@
 'use client';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
-  },
-});
-
 export default function QueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }

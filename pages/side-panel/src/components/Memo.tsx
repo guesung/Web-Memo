1b66862f2404ae22ad004560333b676cff7d5fcf
guesung <gueit214@naver.com>
2024-10-05 12:33:53 +0900

    fix: 상태로 메모를 관리한다

--- pages/side-panel/src/components/Memo.tsx
+++ pages/side-panel/src/components/Memo.tsx
@@ -1,5 +1,5 @@
 import { overlay } from 'overlay-kit';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TopRightArrow } from '../icons';
 
 import { WEB_URL } from '@extension/shared/constants';
@@ -11,17 +11,16 @@ import {
   useThrottle,
   useUserPreferDarkMode,
 } from '@extension/shared/hooks';
+import { MemoSupabaseResponse } from '@extension/shared/types';
 import { getFormattedMemo, I18n, responseUpdateSidePanel, Tab } from '@extension/shared/utils/extension';
 import { Toast } from '@extension/ui';
 import withAuthentication from '@src/hoc/withAuthentication';
 import { UseQueryResult } from '@tanstack/react-query';
-import { MemoSupabaseResponse } from '@extension/shared/types';
 
 function Memo() {
   const [isSaved, setIsSaved] = useState(true);
-  const memoRef = useRef<HTMLTextAreaElement>(null);
-  const getMemoValue = useCallback(() => memoRef?.current?.value ?? '', [memoRef]);
-  const { throttle } = useThrottle();
+  const [memo, setMemo] = useState('');
+  const { throttle, abortThrottle } = useThrottle();
   const { data: tab, refetch: refetchTab } = useTabQuery();
   // TODO :타입 에러로 인해 타입 단언으로 일단 해결
   const { data: memoList }: UseQueryResult<MemoSupabaseResponse, Error> = useMemoListQuery();
@@ -33,29 +32,35 @@ function Memo() {
     responseUpdateSidePanel(() => {
       setIsSaved(true);
       refetchTab();
+      abortThrottle();
     }),
   );
 
   useEffect(() => {
-    if (!memoRef.current || !tab?.url) return;
-    memoRef.current.value = memoList?.data?.find(memo => memo.url === tab.url)?.memo ?? '';
+    if (!tab?.url) return;
+    const currentMemo = memoList?.data?.find(memo => memo.url === tab.url)?.memo ?? '';
+
+    setMemo(currentMemo);
   }, [memoList, tab?.url]);
 
   const handleMemoClick = () => {
     Tab.create({ url: `${WEB_URL}/memos` });
   };
 
-  const handleTextAreaChange = async () => {
-    if (isSaved) return;
+  const handleTextAreaChange = async (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setIsSaved(false);
-    const formattedMemo = await getFormattedMemo(getMemoValue());
-    throttle(() => mutateMemo(formattedMemo));
+    setMemo(event.target.value);
+    const formattedMemo = await getFormattedMemo(memo);
+    throttle(() => {
+      if (isSaved) return;
+      mutateMemo(formattedMemo);
+    });
   };
 
   const handleTextAreaKeyDown = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.metaKey && e.key === 's') {
       e.preventDefault();
-      const formattedMemo = await getFormattedMemo(getMemoValue());
+      const formattedMemo = await getFormattedMemo(memo);
       mutateMemo(formattedMemo, {
         onSuccess: () => {
           overlay.open(({ unmount }) => <Toast message={I18n.get('toast_saved')} onClose={unmount} />);
@@ -67,7 +72,7 @@ function Memo() {
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formattedMemo = await getFormattedMemo(getMemoValue());
+    const formattedMemo = await getFormattedMemo(memo);
     mutateMemo(formattedMemo, {
       onSuccess: () => {
         overlay.open(({ unmount }) => <Toast message={I18n.get('toast_saved')} onClose={unmount} />);
@@ -97,7 +102,7 @@ function Memo() {
         placeholder="memo"
         onChange={handleTextAreaChange}
         onKeyDown={handleTextAreaKeyDown}
-        ref={memoRef}
+        value={memo}
       />
       <div className="label">
         {isSaved ? <span /> : <span className="loading loading-ring loading-xs" />}

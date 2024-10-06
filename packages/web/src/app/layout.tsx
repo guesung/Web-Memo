a64500448c8bfdb909efb29a75d7793a586d0c3f
guesung <gueit214@naver.com>
2024-10-06 16:15:09 +0900

    Feature: Pretendard폰트를 적용한다 (#70)
    * feat: extension에 폰트를 적용한다
    
    * feat: web에 폰트를 적용한다

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,11 +1,20 @@
 import { Inter } from 'next/font/google';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import localFont from 'next/font/local';
+
 import './globals.css';
 import { Header, QueryProvider } from '@src/components';
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const pretendard = localFont({
+  src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: '웹 메모',
@@ -15,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className="bg-base-100 h-screen" data-theme="cupcake">
-      <body className={`${inter.className} h-full`}>
+      <body className={`${pretendard.variable} font-pretendard h-full`}>
         <QueryProvider>
           <Header />
           {children}

16543de4ffd87c77455286cbf0ffd126483ab050
guesung <gueit214@naver.com>
2024-10-07 19:25:56 +0900

    feat: 토큰명을 상수화한다

--- packages/web/src/app/auth/callback/route.ts
+++ packages/web/src/app/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { getSupabaseClient } from '@src/utils/supabase.server';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { COOKIE_KEY_ACCESS_TOKEN, COOKIE_KEY_REFRESH_TOKEN } from '@extension/shared/constants';
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
@@ -13,10 +14,10 @@ export async function GET(request: Request) {
     if (!sessionData.session) throw new Error('no session');
 
     const cookieStore = cookies();
-    cookieStore.set('access_token', sessionData.session.access_token, {
-      maxAge: sessionData.session.expires_in,
+    cookieStore.set(COOKIE_KEY_ACCESS_TOKEN, sessionData.session.access_token, {
+      maxAge: 3600 * 24 * 365, // 1년
     });
-    cookieStore.set('refresh_token', sessionData.session.refresh_token, {
+    cookieStore.set(COOKIE_KEY_REFRESH_TOKEN, sessionData.session.refresh_token, {
       maxAge: 3600 * 24 * 365, // 1년
     });
   }

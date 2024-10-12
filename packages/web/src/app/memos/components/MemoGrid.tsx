bc40a205495e76c90a69ce18f368c4e134bd1296
guesung <gueit214@naver.com>
2024-10-12 16:29:59 +0900

    feat: react-infinitegrid를 적용한다

--- packages/web/src/app/memos/components/MemoGrid.tsx
+++ packages/web/src/app/memos/components/MemoGrid.tsx
@@ -1,9 +1,35 @@
 'use client';
 
 import { useMemoListQuery } from '@extension/shared/hooks';
-import type { MemoRow, MemoSupabaseResponse } from '@extension/shared/types';
+
 import { getSupabaseClient } from '@src/utils/supabase.client';
 import { UseQueryResult } from '@tanstack/react-query';
+import { MasonryInfiniteGrid } from '@egjs/react-infinitegrid';
+import { useEffect, useState } from 'react';
+
+import { MemoRow, MemoSupabaseResponse } from '@extension/shared/types';
+
+const Item = ({ num }: any) => (
+  <div
+    style={{
+      width: '250px',
+    }}>
+    <div className="thumbnail">
+      <img src={`https://naver.github.io/egjs-infinitegrid/assets/image/${(num % 33) + 1}.jpg`} alt="egjs" />
+    </div>
+    <div className="info">{`egjs ${num}`}</div>
+  </div>
+);
+
+function getItems(nextGroupKey: number, count: number) {
+  const nextItems = [];
+  const nextKey = nextGroupKey * count;
+
+  for (let i = 0; i < count; ++i) {
+    nextItems.push({ groupKey: nextGroupKey, key: nextKey + i });
+  }
+  return nextItems;
+}
 
 export default function MemoGrid() {
   const supabaseClient = getSupabaseClient();
@@ -11,16 +37,23 @@ export default function MemoGrid() {
   const { data: memoListData }: UseQueryResult<MemoSupabaseResponse, Error> = useMemoListQuery({
     supabaseClient,
   });
-
   const memoList = memoListData?.data;
+  const [items, setItems] = useState(() => getItems(0, 10));
+
   if (!memoList || memoList.length === 0)
     return <p className="text-center mt-8">아직 저장된 메모가 없어요. 사이드 패널을 열어 메모를 저장해보세요 !</p>;
   return (
-    <section className="break-inside-avoid columns-1 sm:columns-2 lg:columns-3 gap-4 px-4">
-      {memoList.map(memo => (
-        <MemoItem key={memo.id} memo={memo} />
+    <MasonryInfiniteGrid
+      className="container"
+      gap={5}
+      onRequestAppend={e => {
+        const nextGroupKey = (+e.groupKey! || 0) + 1;
+        setItems([...items, ...getItems(nextGroupKey, 10)]);
+      }}>
+      {items.map(item => (
+        <MemoItem data-grid-groupkey={item.groupKey} key={item.key} memo={memoList.at(item.key)} />
       ))}
-    </section>
+    </MasonryInfiniteGrid>
   );
 }
 
@@ -30,10 +63,10 @@ interface MemoItemProps {
 
 function MemoItem({ memo }: MemoItemProps) {
   return (
-    <div className="bg-base-100 shadow-xl mb-4 card box-border">
+    <div className="bg-base-100 shadow-xl mb-4 card box-border w-[250px]">
       <div className="card-body">
-        <p className="font-bold">{memo.title}</p>
-        <p className="whitespace-break-spaces">{memo.memo}</p>
+        <p className="font-bold">{memo?.title}</p>
+        <p className="whitespace-break-spaces">{memo?.memo}</p>
       </div>
     </div>
   );

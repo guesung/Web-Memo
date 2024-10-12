cef549a5f4d322b82e9e2943f22b6a35ccd20791
guesung <gueit214@naver.com>
2024-10-12 18:48:52 +0900

    fix: content-ui 에러를 해결한다

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -1,15 +1,3 @@
-import { isProduction } from '@extension/shared/utils';
-import { OPEN_SIDE_PANEL_ID, responsePageContent } from '@extension/shared/utils/extension';
-import { OpenSidePanelButton } from './components';
-import { attachShadowTree } from './utils';
-
-const renderOpenSidePanelButton = async () => {
-  if (isProduction) return;
-
-  attachShadowTree({
-    shadowHostId: OPEN_SIDE_PANEL_ID,
-    shadowTree: <OpenSidePanelButton />,
-  });
-};
+import { responsePageContent } from '@extension/shared/utils/extension';
 
 responsePageContent();

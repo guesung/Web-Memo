113a27d4ad0afbeff95d261fe2175c34870111d2
guesung <gueit214@naver.com>
2024-10-13 13:20:13 +0900

    refactor: url이 없을 때에만 에러를 던진다

--- packages/shared/src/utils/extension/memo.ts
+++ packages/shared/src/utils/extension/memo.ts
@@ -9,11 +9,11 @@ export interface GetFormattedMemoProps {
 export const getFormattedMemo = async ({ memo, category }: GetFormattedMemoProps) => {
   const tab = await Tab.get();
 
-  if (!tab.url || !tab.title || !tab.favIconUrl) throw new Error('Save Failed: Invalid URL');
+  if (!tab.url) throw new Error('Save Failed: Invalid URL');
 
   return {
     title: tab.title,
-    favIconUrl: tab.favIconUrl,
+    favIconUrl: tab?.favIconUrl,
     url: formatUrl(tab.url),
     memo,
     category,

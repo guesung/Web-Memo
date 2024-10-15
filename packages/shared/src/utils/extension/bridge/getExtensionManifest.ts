379085831d8de3ff3c7f1615a1a3c2dfbe6b25c4
guesung <gueit214@naver.com>
2024-10-15 13:23:31 +0900

    feat: 로그인 화면에서, 설치된 버전을 보여준다

--- packages/shared/src/utils/extension/bridge/getExtensionManifest.ts
+++ packages/shared/src/utils/extension/bridge/getExtensionManifest.ts
@@ -2,8 +2,9 @@ import { EXTENSION_ID } from '@src/constants';
 import { Runtime } from '../module';
 
 export const BRIDGE_TYPE_GET_EXTENSION_MANIFEST = 'BRIDGE_TYPE_GET_EXTENSION_MANIFEST';
-export const requestGetExtensionManifest = (callbackFn: (response: chrome.runtime.Manifest) => void) =>
+export const requestGetExtensionManifest = (callbackFn: (response: chrome.runtime.Manifest) => void) => {
   chrome.runtime.sendMessage(EXTENSION_ID, { type: BRIDGE_TYPE_GET_EXTENSION_MANIFEST }, callbackFn);
+};
 export const responseGetExtensionManifest = () => {
   const manifest = chrome.runtime.getManifest();
   Runtime.onMessageExternal(BRIDGE_TYPE_GET_EXTENSION_MANIFEST, (_, __, sendResponse) => sendResponse(manifest));
---
+++ packages/web/src/app/login/components/ExtensionVersion.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { URL_CHROME_STORE } from '@extension/shared/constants';
+import { useDidMount } from '@extension/shared/hooks';
+import { requestGetExtensionManifest } from '@extension/shared/utils/extension';
+import { useState } from 'react';
+
+export default function ExtensionVersion() {
+  const [version, setVersion] = useState<null | string>(null);
+
+  useDidMount(() => {
+    try {
+      requestGetExtensionManifest(manifest => {
+        setVersion(manifest.version);
+        console.log(manifest.version);
+      });
+    } catch (e) {
+      const answer = window.confirm(
+        '익스텐션을 설치하지 않으셨군요. 설치해야 메모 기능을 이용하실 수 있습니다. 설치하러 가시겠습니까?',
+      );
+      if (!answer) return;
+      location.href = URL_CHROME_STORE;
+    }
+  });
+
+  if (version)
+    return <p className="w-full text-sm absolute right-2 bottom-2 text-gray-600 text-end">설치된 버전 : {version}</p>;
+  return;
+}

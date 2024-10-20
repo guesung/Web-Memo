27fbb792f086d9bbd63cd2dbfc872673de4538f1
guesung <gueit214@naver.com>
2024-10-20 13:10:35 +0900

    Feature: 로그인 페이지에 그라데이션 추가 (#89)
    * feat: `uvcanvas` 설치
    
    * feat: 로그인 페이지에 `Lumiflex` 추가

--- packages/web/src/app/login/components/LoginSection.tsx
+++ packages/web/src/app/login/components/LoginSection.tsx
@@ -4,18 +4,18 @@ import ExtensionVersion from './ExtensionVersion';
 
 export default async function LoginSection() {
   return (
-    <section className="flex justify-center flex-col items-center card w-96 shadow-xl base-300 py-12">
-      <p className="text-2xl text-center">환영합니다</p>
+    <section className="card base-300 flex w-96 flex-col items-center justify-center bg-white py-12 opacity-80 shadow-xl">
+      <p className="text-center text-2xl">환영합니다</p>
       <p className="text-md text-center">편리하게 메모를 작성할 수 있는 웹 메모입니다.</p>
       <div className="h-8" />
-      <form className="flex flex-col gap-4 w-full px-4">
+      <form className="flex w-full flex-col gap-4 px-4">
         <button
           formAction={signInWithOAuth.bind(null, 'kakao')}
           className="btn bg-[rgb(247,228,76)] text-black hover:bg-[rgb(247,228,76)]">
           <Image src="/images/svgs/kakao.svg" width={16} height={16} alt="google" />
           카카오 로그인
         </button>
-        <button formAction={signInWithOAuth.bind(null, 'google')} className="btn bg-white hover:bg-white text-black">
+        <button formAction={signInWithOAuth.bind(null, 'google')} className="btn bg-white text-black hover:bg-white">
           <Image src="/images/svgs/google.svg" width={16} height={16} alt="google" />
           구글 로그인
         </button>

8571f7006aae8b6ed9216d16bc4eadf84874cad3
guesung <gueit214@naver.com>
2024-10-21 15:21:02 +0900

    chore: 헤더야 `ThemeController` 추가

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -4,24 +4,26 @@ import { MemoRefresh } from '@src/app/memos/components';
 import { getSupabaseClient, signout } from '@src/utils/supabase.server';
 import Image from 'next/image';
 import Link from 'next/link';
+import { ThemeController } from '@extension/ui';
 
 export default async function Header() {
   const supabaseClient = getSupabaseClient();
   const user = await getUser(supabaseClient);
 
   return (
-    <header className="navbar bg-base-100 flex-1 w-full fixed inset-x-0 shadow-sm z-50">
+    <header className="navbar bg-base-100 fixed inset-x-0 z-50 w-full flex-1 shadow-sm">
       <div className="navbar-start">
         <Link href="/memos" className="btn btn-ghost">
           <Image src="/images/pngs/icon.png" width={28} height={28} alt="icon" />
           <span className="text-xl">웹 메모</span>
         </Link>
       </div>
-      <div className="flex navbar-end">
+      <div className="navbar-end flex">
         {user?.data?.user ? (
           <div className="flex gap-2">
+            <ThemeController />
             <button className="btn btn-circle btn-sm">
-              <MemoRefresh width={32} hanging={32} fill="white" id="refresh" cursor="pointer" color="gray" />
+              <MemoRefresh width={32} hanging={32} id="refresh" cursor="pointer" color="gray" />
             </button>
             <div className="btn btn-circle btn-sm">
               <div className="avatar dropdown dropdown-bottom dropdown-end">

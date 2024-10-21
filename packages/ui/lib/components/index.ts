13cab0e388693b02e66db9ea45f10befb108c29c
guesung <gueit214@naver.com>
2024-10-21 15:20:26 +0900

    feat: 테마를 설정하는 `ThemeController`구현

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -3,3 +3,4 @@ export { default as Loading } from './Loading';
 export { default as ErrorFallback } from './ErrorFallback';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as LoadingFallback } from './LoadingFallback';
+export { default as ThemeController } from './ThemeController';

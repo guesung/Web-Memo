985671b09d8bf25d27c85fe0305406f8d8a14d90
guesung <gueit214@naver.com>
2024-10-27 14:13:16 +0900

    fix: url 비교할 때 `formatUrl`로 해시 제거

--- packages/shared/src/hooks/useMemoQuery.ts
+++ packages/shared/src/hooks/useMemoQuery.ts
@@ -1,6 +1,6 @@
 import { queryKeys } from '@src/constants';
 import { MemoSupabaseClient, MemoSupabaseResponse } from '@src/types';
-import { getMemoSupabase } from '@src/utils';
+import { formatUrl, getMemoSupabase } from '@src/utils';
 import { useQuery } from '@tanstack/react-query';
 
 interface UseMemoQueryProps {
@@ -14,7 +14,7 @@ export default function useMemoQuery({ supabaseClient, url }: UseMemoQueryProps)
     queryKey: queryKeys.memoList(),
     enabled: !!supabaseClient,
     select: ({ data: memoList }: MemoSupabaseResponse) => {
-      const currentMemo = memoList?.find(memo => memo.url === url);
+      const currentMemo = memoList?.find(memo => memo.url === formatUrl(url));
       return currentMemo;
     },
   });

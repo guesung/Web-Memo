2eaf3c927068daa8999b8bce56b564b23f62295e
guesung <gueit214@naver.com>
2024-10-28 21:29:06 +0900

    refactor: 훅을 각 사용하는 측에 맞게 폴더로 묶기

--- packages/shared/src/hooks/index.ts
+++ packages/shared/src/hooks/index.ts
@@ -3,10 +3,6 @@ export { default as useFetch } from './useFetch';
 export { default as useDidMount } from './useDidMount';
 export { default as useThrottle } from './useThrottle';
 export { default as useUserPreferDarkMode } from './useUserPreferDarkMode';
-export { default as useTabQuery } from './useTabQuery';
-export { default as useMemoPostMutation } from './useMemoPostMutation';
-export { default as useMemoPatchMutation } from './useMemoPatchMutation';
-export { default as useMemoListQuery } from './useMemoListQuery';
-export { default as useSupabaseClient } from './useSupabaseClient';
-export { default as useSupabaseUser } from './useSupabaseUser';
-export { default as useMemoQuery } from './useMemoQuery';
+
+export * from './supabase';
+export * from './extension';

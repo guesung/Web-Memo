b0066e0b65376dde6d9c847da13bfca5b1c20a8b
guesung <gueit214@naver.com>
2024-11-12 16:00:48 +0900

    refactor: 정규표현식 상수로 그룹화

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,10 +5,12 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
+  timeout: 30000,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
+    screenshot: process.env.CI ? undefined : 'only-on-failure',
   },
   projects: [
     {
--- tests/e2e/LoginPage.spec.ts
+++ tests/e2e/LoginPage.spec.ts
@@ -1,27 +1,42 @@
 import { expect, test } from './fixtures';
 
+const REGEXR = {
+  page: {
+    login: /.*login/,
+    kakaoLogin: /https:\/\/accounts.kakao.com*/,
+    googleLogin: /https:\/\/accounts.google.com\/v3\/signin*/,
+    memos: /.*memos/,
+  },
+};
+
 test.describe('Login Page', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000');
   });
   test.describe('로그인', () => {
     test('로그인을 하지 않고 접속할 경우, login페이지로 redirect된다.', async ({ page }) => {
-      expect(page).toHaveURL(/.*login/);
+      expect(page).toHaveURL(REGEXR.page.login);
     });
     test('카카오 로그인 버튼을 클릭하면 카카오 로그인 페이지로 이동한다.', async ({ page }) => {
-      page.getByRole('button', { name: 'kakao 카카오 로그인' }).click();
+      await page.getByRole('button', { name: 'kakao 카카오 로그인' }).click();
 
-      expect(page).toHaveURL(/https:\/\/accounts.kakao.com*/);
+      await page.waitForURL(REGEXR.page.kakaoLogin);
+
+      expect(page).toHaveURL(REGEXR.page.kakaoLogin);
     });
     test('구글 로그인 버튼을 클릭하면 구글 로그인 페이지로 이동한다.', async ({ page }) => {
       await page.getByRole('button', { name: 'google 구글 로그인' }).click();
 
-      expect(page).toHaveURL(/https:\/\/accounts.google.com\/v3\/signin*/);
+      await page.waitForURL(REGEXR.page.googleLogin);
+
+      expect(page).toHaveURL(REGEXR.page.googleLogin);
     });
     test('테스트 계정으로 로그인 시, memos페이지로 이동한다.', async ({ page }) => {
       await page.getByRole('button', { name: '테스트 계정으로 로그인' }).click();
 
-      expect(page).toHaveURL(/.*memos/);
+      await page.waitForURL(REGEXR.page.memos);
+
+      expect(page).toHaveURL(REGEXR.page.memos);
     });
   });
 });

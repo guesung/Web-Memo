7d7655918a38ce0ff6e58296aed07eae6fde2083
guesung <gueit214@naver.com>
2024-11-14 22:41:17 +0900

    refactor: svg로부터 직접 import

--- pages/side-panel/src/components/MemoHeader.tsx
+++ pages/side-panel/src/components/MemoHeader.tsx
@@ -1,7 +1,8 @@
-import { useDidMount, useTabQuery, useUserPreferDarkMode } from '@extension/shared/hooks';
-import { TopRightArrow } from '../icons';
 import { WEB_URL } from '@extension/shared/constants';
+import { useDidMount, useTabQuery, useUserPreferDarkMode } from '@extension/shared/hooks';
 import { I18n, responseUpdateSidePanel, Tab } from '@extension/shared/utils/extension';
+import Plus from '../../public/svgs/plus.svg';
+import TopRightArrow from '../../public/svgs/top_right_arrow.svg';
 
 export default function MemoHeader() {
   const { data: tab, refetch: refetchTab } = useTabQuery();
@@ -27,6 +28,7 @@ export default function MemoHeader() {
         onClick={handleMemoClick}
         className="cursor-pointer"
       />
+      <Plus width={20} height={20} className="cursor-pointer" fill={isUserPreferDarkMode ? 'black' : 'white'} />
       <span className="w-4" />
       <span className="label-text w-full truncate text-right">{tab?.title}</span>
     </div>
--- pages/side-panel/src/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Refresh } from './Refresh';
-export { default as Option } from './Option';
-export { default as TopRightArrow } from './TopRightArrow';
-export { default as Plus } from './Plus';

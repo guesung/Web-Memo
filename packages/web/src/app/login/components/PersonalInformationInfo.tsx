98de60813ef733c60d5de0c8e7f099317dcea443
guesung <gueit214@naver.com>
2024-11-14 14:54:16 +0900

    refactor: 불필요한 파일 제거

--- packages/web/src/app/login/components/PersonalInformationInfo.tsx
+++ packages/web/src/app/login/components/PersonalInformationInfo.tsx
@@ -2,7 +2,6 @@ export default function PersonalInformationInfo() {
   return (
     <footer className="absolute inset-x-0 bottom-4 text-center text-sm">
       <p>귀하의 데이터는 절대 공유되지 않습니다. 스팸 메시지가 없습니다.</p>
-      {/* <p>계속함으로써, 이용 약관 및 개인정보 처리방침에 동의하게 됩니다</p> */}
     </footer>
   );
 }

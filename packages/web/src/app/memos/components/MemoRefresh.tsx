a54f4cc9b9fe118263f5ce2e1b7f0e92f30eefc9
guesung <gueit214@naver.com>
2024-11-14 14:00:17 +0900

    refactor: Refresh svg 분리

--- packages/web/src/app/memos/components/MemoRefresh.tsx
+++ packages/web/src/app/memos/components/MemoRefresh.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { queryKeys } from '@extension/shared/constants';
 import { useQueryClient } from '@tanstack/react-query';
-import React from 'react';
 
+import { HTMLMotionProps, motion } from 'framer-motion';
 import { toast } from 'react-toastify';
-import { motion, SVGMotionProps } from 'framer-motion';
 import { driverObj } from '../utils';
+import Image from 'next/image';
 
-interface MemoRefreshProps extends SVGMotionProps<SVGSVGElement> {}
+interface MemoRefreshProps extends HTMLMotionProps<'div'> {}
+
 export default function MemoRefresh({ ...props }: MemoRefreshProps) {
   const queryClient = useQueryClient();
   const handleClick = async () => {
@@ -19,23 +20,8 @@ export default function MemoRefresh({ ...props }: MemoRefreshProps) {
   };
 
   return (
-    <motion.svg
-      width="current"
-      height="current"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      onClick={handleClick}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      {...props}>
-      <path
-        d="M21 3V8M21 8H16M21 8L18 5.29168C16.4077 3.86656 14.3051 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.2832 21 19.8675 18.008 20.777 14"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </motion.svg>
+    <motion.div onClick={handleClick} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} {...props}>
+      <Image src="/images/svgs/refresh.svg" alt="refresh" width={16} height={16} />
+    </motion.div>
   );
 }

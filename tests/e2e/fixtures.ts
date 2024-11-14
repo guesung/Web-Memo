cdda8a77770a0929b566502a6b69cc040328a828
guesung <gueit214@naver.com>
2024-11-14 21:50:49 +0900

    feat: headless 옵션 활성화

--- tests/e2e/fixtures.ts
+++ tests/e2e/fixtures.ts
@@ -15,7 +15,7 @@ export const test = base.extend<ExtensionFixture>({
   context: async ({}, use) => {
     const context = await chromium.launchPersistentContext('', {
       headless: false,
-      args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
+      args: [`--headless=new`, `--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
     });
     await use(context);
     await context.close();

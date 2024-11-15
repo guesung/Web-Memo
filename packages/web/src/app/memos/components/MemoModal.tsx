670db52f635d4d1943b904829a3da698f2153478
guesung <gueit214@naver.com>
2024-11-15 16:47:25 +0900

    feat: textarea으로 텍스트 수정할 수 있도록 수정

--- packages/web/src/app/memos/components/MemoModal.tsx
+++ packages/web/src/app/memos/components/MemoModal.tsx
@@ -10,24 +10,37 @@ import {
 } from '@src/components/ui/dialog';
 import { useMemoQuery, useSupabaseClient } from '@extension/shared/hooks';
 import { getSupabaseClient } from '@src/utils/supabase.client';
+import { Textarea } from '@src/components/ui/textarea';
+import { useEffect, useState } from 'react';
 
 export default function MemoModal() {
   const searchParams = useSearchParams();
   const id = searchParams.get('id') ?? '';
   const router = useRouter();
+  const [row, setRow] = useState(4);
 
-  const { data: supabaseClient } = useSupabaseClient({ getSupabaseClient });
+  const supabaseClient = getSupabaseClient();
   const { data: memoData } = useMemoQuery({ supabaseClient, id });
+
+  useEffect(() => {
+    if (!memoData?.memo) return;
+
+    const rowCount = memoData?.memo.split(/\r\n|\r|\n/).length;
+
+    setRow(rowCount + 2);
+  }, [memoData]);
 
   const handleClose = () => {
     router.replace('/memos', { scroll: false });
   };
+
+  if (!id) return;
   return (
     <Dialog open={!!id}>
       <DialogContent onClose={handleClose}>
         <DialogHeader>
           <DialogTitle>{memoData?.title}</DialogTitle>
-          <DialogDescription>{memoData?.memo}</DialogDescription>
+          <Textarea defaultValue={memoData?.memo} rows={row} />
         </DialogHeader>
       </DialogContent>
     </Dialog>

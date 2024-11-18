aae0a29d4bd2a3999bd4fcc8b4472d8f67c8452b
guesung <gueit214@naver.com>
2024-11-18 13:00:43 +0900

    chore: name 필드 not null로 수정

--- packages/shared/src/types/supabase.ts
+++ packages/shared/src/types/supabase.ts
@@ -7,19 +7,19 @@ export type Database = {
         Row: {
           created_at: string;
           id: number;
-          name: string | null;
+          name: string;
           user_id: string | null;
         };
         Insert: {
           created_at?: string;
           id?: number;
-          name?: string | null;
+          name: string;
           user_id?: string | null;
         };
         Update: {
           created_at?: string;
           id?: number;
-          name?: string | null;
+          name?: string;
           user_id?: string | null;
         };
         Relationships: [];

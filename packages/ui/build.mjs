223b03f9981382f4d66c77bc96ea04d6284abab1
guesung <gueit214@naver.com>
2024-11-19 13:08:09 +0900

    feat: ui패키지 세팅

--- packages/ui/build.mjs
+++ packages/ui/build.mjs
@@ -1,18 +1,36 @@
 import * as esbuild from 'esbuild';
 import * as fs from 'fs';
 import { resolve } from 'node:path';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  * @type { import("esbuild").BuildOptions }
  */
 const buildOptions = {
-  entryPoints: ['./index.ts', './tailwind.config.ts', './lib/**/*.ts', './lib/**/*.tsx'],
+  entryPoints: ['./index.ts', './tailwind.config.ts', './lib/**/*', './src/**/*'],
   tsconfig: './tsconfig.json',
-  bundle: false,
+  bundle: true,
   target: 'es6',
   outdir: './dist',
   sourcemap: true,
+  alias: {
+    '@': resolve(__dirname, './src'),
+  },
 };
 
-await esbuild.build(buildOptions);
-fs.copyFileSync(resolve('lib', 'global.css'), resolve('dist', 'global.css'));
+const build = async () => {
+  await esbuild.build(buildOptions);
+  fs.copyFileSync(resolve('lib', 'global.css'), resolve('dist', 'global.css'));
+
+  const execAsync = promisify(exec);
+  await execAsync('tsc --emitDeclarationOnly --outDir dist');
+};
+
+build().catch(error => {
+  console.log(error);
+  process.exit(1);
+});
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/components';
-export * from './lib/utils';
 export * from './lib/withUI';
+export * from './src/components';
+export * from './src/lib';

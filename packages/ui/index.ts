b1fb78d4897ac54b23834176ff45cfe53fe6f98f
guesung <gueit214@naver.com>
2024-11-19 14:36:02 +0900

    refactor: lib폴더 제거하고 src로 통일

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/components';
 export * from './src/components';
-export * from './src/lib';
+export * from './src/utils';
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,1 +1,6 @@
 export * from './button';
+export { default as Toast } from './Toast';
+export { default as Loading } from './Loading';
+export { default as ErrorFallback } from './ErrorFallback';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as LoadingFallback } from './LoadingFallback';

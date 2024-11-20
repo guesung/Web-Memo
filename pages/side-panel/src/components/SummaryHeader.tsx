031452761e7d2bd8a982f5c2a5d1ee861e005b5c
guesung <gueit214@naver.com>
2024-11-20 21:40:31 +0900

    chore: 테마 설정에 대한 에러 처리

--- pages/side-panel/src/components/SummaryHeader.tsx
+++ pages/side-panel/src/components/SummaryHeader.tsx
@@ -1,4 +1,4 @@
-import { Button, Loading } from '@extension/ui';
+import { Button, ErrorBoundary, Loading } from '@extension/ui';
 import { RefreshCwIcon } from 'lucide-react';
 import { useSummaryContext } from './SummaryProvider';
 import ToggleTheme from './ToggleTheme';
@@ -8,7 +8,9 @@ export default function Header() {
 
   return (
     <header className="float-right mt-4 flex items-center gap-1">
-      <ToggleTheme />
+      <ErrorBoundary>
+        <ToggleTheme />
+      </ErrorBoundary>
       {isSummaryLoading ? (
         <Button variant="outline" size="icon">
           <Loading />

60ac685cf8448fa639c0dab56c2486846d88a465
guesung <gueit214@naver.com>
2024-11-20 14:01:45 +0900

    chore: 옵션을 임시로 제거

--- pages/side-panel/src/components/SummaryHeader.tsx
+++ pages/side-panel/src/components/SummaryHeader.tsx
@@ -1,18 +1,13 @@
 import { Loading } from '@extension/ui';
-import { RefreshCwIcon, SettingsIcon } from 'lucide-react';
+import { RefreshCwIcon } from 'lucide-react';
 import { useSummaryContext } from './SummaryProvider';
 
 export default function Header() {
   const { isSummaryLoading, refetchSummary } = useSummaryContext();
 
-  const handleOptionClick = () => {
-    chrome.runtime.openOptionsPage();
-  };
-
   return (
     <header className="float-right mt-4 flex items-center gap-1">
       {isSummaryLoading ? <Loading /> : <RefreshCwIcon size={16} onClick={refetchSummary} role="button" />}
-      <SettingsIcon size={16} onClick={handleOptionClick} role="button" />
     </header>
   );
 }

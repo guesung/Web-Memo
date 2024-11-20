64c5416d833d5e77df6ac5a01697e696f0491f10
guesung <gueit214@naver.com>
2024-11-20 16:54:18 +0900

    fix: 대문자 `Toast`와 `Loading` 제거

--- packages/ui/src/components/Loading.tsx
+++
@@ -1,6 +0,0 @@
-import { LoaderCircle } from 'lucide-react';
-
-interface LoadingProps extends React.ComponentProps<'svg'> {}
-export function Loading(props: LoadingProps) {
-  return <LoaderCircle className="animate-spin" size={16} {...props} />;
-}

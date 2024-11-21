e7121d37f9b179481e28bfb1ff7b79d935ed7742
guesung <gueit214@naver.com>
2024-11-21 19:20:55 +0900

    fix: 라우팅 안되는 이슈 해결

--- packages/web/src/constants/Path.ts
+++ packages/web/src/constants/Path.ts
@@ -1,12 +1,12 @@
 export const PATHS = {
   root: '/',
-  error: '/error',
-  login: '/login',
-  memos: '/memos',
-  memosWish: '/memos?wish=true',
-  memosSetting: '/memos/setting',
-  allbackoAuth: '/auth/callback',
-  callbackEmail: '/auth/callback-email',
+  error: 'error',
+  login: 'login',
+  memos: 'memos',
+  memosWish: 'memos?wish=true',
+  memosSetting: 'memos/setting',
+  allbackoAuth: 'auth/callback',
+  callbackEmail: 'auth/callback-email',
 };
 
 export const NEED_AUTH_PAGES = [PATHS.memos, PATHS.memosSetting];

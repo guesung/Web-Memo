3313169df2f15d0635fb9e6ec6b32067207fac68
guesung <gueit214@naver.com>
2024-11-21 13:39:41 +0900

    feat: i18next 설정
    ref : https://locize.com/blog/next-app-dir-i18n/

--- packages/web/src/constants/index.ts
+++ packages/web/src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './Config';
 export * from './Page';
+export * from './I18n';
--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,8 +1,38 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { updateSession } from './utils';
+import acceptLanguage from 'accept-language';
+import { fallbackLng, languages, cookieName } from './constants';
+
+acceptLanguage.languages(languages);
+
+let locales = ['en', 'ko'];
+
+const getLocale = (request: NextRequest) => 'ko';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  let lng;
+  if (request.cookies.has(cookieName)) lng = acceptLanguage.get(request.cookies.get(cookieName)?.value);
+  if (!lng) lng = acceptLanguage.get(request.headers.get('Accept-Language'));
+  if (!lng) lng = fallbackLng;
+
+  if (
+    !languages.some(language => request.nextUrl.pathname.startsWith(`/${language}`)) &&
+    !request.nextUrl.pathname.startsWith('/_next')
+  ) {
+    return NextResponse.redirect(new URL(`/${lng}${request.nextUrl.pathname}`, request.url));
+  }
+
+  if (request.headers.has('referer')) {
+    const refererUrl = new URL(request.headers.get('referer') ?? '');
+    const lngInReferer = languages.find(l => refererUrl.pathname.startsWith(`/${l}`));
+    const response = NextResponse.next();
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    return response;
+  }
+
+  await updateSession(request);
+
+  return NextResponse.next();
 }
 
 export const config = {

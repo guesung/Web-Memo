bc3812e74b9a26c3d8b15a98afa5cddfdae5309a
guesung <gueit214@naver.com>
2024-11-22 12:34:00 +0900

    refactor: 상수 `PATHS.memos` 로 수정

--- packages/web/src/app/[lng]/login/page.tsx
+++ packages/web/src/app/[lng]/login/page.tsx
@@ -3,13 +3,14 @@ import { getSupabaseClient } from '@src/utils/supabase.server';
 import { LoginSection, PersonalInformationInfo } from './components';
 import { redirect } from 'next/navigation';
 import { LanguageParams } from '@src/app/i18n/type';
+import { PATHS } from '@src/constants';
 
 export default async function page({ params: { lng } }: LanguageParams) {
   const supabaseClient = getSupabaseClient();
   const user = await supabaseClient.auth.getUser();
   const isUserLogin = !!user?.data?.user;
 
-  if (isUserLogin) redirect('/memos');
+  if (isUserLogin) redirect(PATHS.memos);
   return (
     <main className="relative flex h-full items-center justify-center">
       <LoginSection lng={lng} />

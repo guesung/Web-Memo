04950a41aa2b27f1cfe74155813b54cee80661ee
guesung <gueit214@naver.com>
2024-11-26 14:57:15 +0900

    refactor: Map과 Set을 활용하여 리팩토링

--- packages/web/src/utils/SearchParams.ts
+++ packages/web/src/utils/SearchParams.ts
@@ -2,50 +2,55 @@ import { SearchParamKeyType, SearchParamType, SearchParamValueType } from '@src/
 import { ReadonlyURLSearchParams } from 'next/navigation';
 
 export class SearchParams {
-  #searchParams: SearchParamType[];
+  #searchParamsMap: Map<SearchParamKeyType, Set<SearchParamValueType>>;
 
   constructor(searchParams: SearchParamType[]) {
-    this.#searchParams = searchParams;
+    this.#searchParamsMap = new Map();
+    searchParams.forEach(([key, value]) => {
+      if (!this.#searchParamsMap.has(key)) {
+        this.#searchParamsMap.set(key, new Set());
+      }
+      this.#searchParamsMap.get(key)?.add(value);
+    });
   }
 
   get = (key: SearchParamKeyType) => {
-    return this.#searchParams.find(([searchParamKey]) => key === searchParamKey)?.[1] ?? '';
+    const values = this.#searchParamsMap.get(key);
+    return values ? Array.from(values)[0] || '' : '';
   };
 
   getAll = (key: SearchParamKeyType) => {
-    return this.#searchParams
-      .filter(([searchParamKey]) => key === searchParamKey)
-      .map(([searchParamKey, searchParamValue]) => searchParamValue);
+    return Array.from(this.#searchParamsMap.get(key) || []);
   };
 
-  add = ([searchParamKey, searchParamValue]: SearchParamType) => {
-    this.#searchParams = this.#searchParams.concat([[searchParamKey, searchParamValue]]);
+  add = ([key, value]: SearchParamType) => {
+    if (!this.#searchParamsMap.has(key)) {
+      this.#searchParamsMap.set(key, new Set());
+    }
+    this.#searchParamsMap.get(key)?.add(value);
   };
 
-  set = ([searchParamKey, searchParamValue]: SearchParamType) => {
-    this.#searchParams = this.#searchParams
-      .filter(([key]) => key !== searchParamKey)
-      .concat([[searchParamKey, searchParamValue]]);
+  set = ([key, value]: SearchParamType) => {
+    this.#searchParamsMap.set(key, new Set([value]));
   };
 
-  remove = ([searchParamKey, searchParamValue]: SearchParamType) => {
-    this.#searchParams = this.#searchParams.filter(
-      ([key, value]) => !(key === searchParamKey && value === searchParamValue),
-    );
+  remove = ([key, value]: SearchParamType) => {
+    this.#searchParamsMap.get(key)?.delete(value);
+    if (this.#searchParamsMap.get(key)?.size === 0) {
+      this.#searchParamsMap.delete(key);
+    }
   };
 
-  removeAll = (searchParamKey: SearchParamKeyType) => {
-    this.#searchParams = this.#searchParams.filter(([key]) => key !== searchParamKey);
+  removeAll = (key: SearchParamKeyType) => {
+    this.#searchParamsMap.delete(key);
   };
 
   getSearchParams() {
-    return this.#createSearchParams();
-  }
+    const params: SearchParamType[] = [];
+    this.#searchParamsMap.forEach((values, key) => {
+      values.forEach(value => params.push([key, value]));
+    });
 
-  #createSearchParams() {
-    return this.#searchParams.reduce(
-      (acc, [key, value], index) => acc + `${index === 0 ? '?' : '&'}${key}=${value}`,
-      '',
-    );
+    return params.reduce((acc, [key, value], index) => acc + `${index === 0 ? '?' : '&'}${key}=${value}`, '');
   }
 }

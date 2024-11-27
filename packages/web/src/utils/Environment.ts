a2a8b6d8d9839c5face3ca5af39d0d6d8dd651cb
guesung <gueit214@naver.com>
2024-11-27 12:44:37 +0900

    fix: Mac인지 여부 더 세밀하게 체크

--- packages/web/src/utils/Environment.ts
+++ packages/web/src/utils/Environment.ts
@@ -1,2 +1,2 @@
 export const isProduction = process.env.NODE_ENV === 'production';
-export const isMac = navigator.userAgent.includes('Macintosh');
+export const isMac = typeof navigator !== 'undefined' && /Mac|iPhone|iPad|iPod/.test(navigator.platform);

27218eac066c14c52f5c6b034fb75bb5fa047c19
guesung <gueit214@naver.com>
2024-11-27 11:11:56 +0900

    refactor: js파일을 ts파일로 수정

---
+++ packages/web/src/modules/i18n/server.ts
@@ -0,0 +1,28 @@
+import { createInstance } from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+import { initReactI18next } from 'react-i18next/initReactI18next';
+import { getOptions } from './util';
+import { Language } from './type';
+
+const initI18next = async (language: Language, ns: string) => {
+  const i18nInstance = createInstance();
+  await i18nInstance
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend((language: Language, namespace: string) => import(`./locales/${language}/${namespace}.json`)),
+    )
+    .init(getOptions(language, ns));
+  return i18nInstance;
+};
+
+interface TranslationOptions {
+  keyPrefix?: string;
+}
+
+export default async function useTranslation(language: Language, ns: string, options: TranslationOptions = {}) {
+  const i18nextInstance = await initI18next(language, ns);
+  return {
+    t: i18nextInstance.getFixedT(language, Array.isArray(ns) ? ns[0] : ns, options.keyPrefix),
+    i18n: i18nextInstance,
+  };
+}

3f57689fedd2a0cfc338fdbbac706c7c373e933c
guesung <gueit214@naver.com>
2024-11-29 12:35:20 +0900

    fix: side panel을 기본값으로 열기

--- packages/web/src/app/[lng]/memos/layout.tsx
+++ packages/web/src/app/[lng]/memos/layout.tsx
@@ -1,12 +1,11 @@
 'use server';
 
-import { COOKIE_KEY, PATHS, QUERY_KEY } from '@extension/shared/constants';
+import { PATHS, QUERY_KEY } from '@extension/shared/constants';
 import { checkUserLogin, getCategories } from '@extension/shared/utils';
 import { Header, HydrationBoundaryWrapper } from '@src/components';
 import { SidebarProvider } from '@src/components/ui/sidebar';
 import { LanguageParams } from '@src/modules/i18n';
 import { getSupabaseClient } from '@src/modules/supabase/util.server';
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
@@ -19,14 +18,11 @@ export default async function Layout({ children, params: { lng } }: LayoutProps)
   const isUserLogin = await checkUserLogin(supabaseClient);
   if (!isUserLogin) redirect(PATHS.login);
 
-  const cookieStore = cookies();
-  const defaultOpen = cookieStore.get(COOKIE_KEY.sideBarState)?.value === 'true';
-
   return (
     <div>
       <Header.Margin />
       <div className="bg-background flex w-full p-4 text-sm">
-        <SidebarProvider defaultOpen={defaultOpen}>
+        <SidebarProvider>
           <HydrationBoundaryWrapper queryKey={QUERY_KEY.category()} queryFn={() => getCategories(supabaseClient)}>
             <MemoSidebar lng={lng} />
           </HydrationBoundaryWrapper>

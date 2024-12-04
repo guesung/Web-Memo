7c4b82e311584c275ec481e4a2ba6ba6bb46b80e
guesung <gueit214@naver.com>
2024-12-04 15:05:24 +0900

    chore: category 스키마 상수 추가

--- packages/shared/src/utils/Supabase.ts
+++ packages/shared/src/utils/Supabase.ts
@@ -33,21 +33,21 @@ export const checkUserLogin = async (supabaseClient: MemoSupabaseClient) => {
 };
 
 export const getCategories = async (supabaseClient: MemoSupabaseClient) =>
-  supabaseClient.from('category').select('*').order('created_at', { ascending: false });
+  supabaseClient.from(SUPABASE.schemaCategory).select('*').order('created_at', { ascending: false });
 
 export const insertCategory = async (supabaseClient: MemoSupabaseClient, categoryRequest: CategoryTable['Insert']) =>
-  supabaseClient.from('category').insert(categoryRequest).select();
+  supabaseClient.from(SUPABASE.schemaCategory).insert(categoryRequest).select();
 
 export const updateCategory = async (
   supabaseClient: MemoSupabaseClient,
   id: CategoryRow['id'],
   categoryRequest: CategoryTable['Update'],
-) => supabaseClient.from('category').update(categoryRequest).eq('id', id).select();
+) => supabaseClient.from(SUPABASE.schemaCategory).update(categoryRequest).eq('id', id).select();
 
 export const deleteCategory = async (supabaseClient: MemoSupabaseClient, id: CategoryRow['id']) =>
-  supabaseClient.from('category').delete().eq('id', id).select();
+  supabaseClient.from(SUPABASE.schemaCategory).delete().eq('id', id).select();
 
 export const upsertCategories = async (
   supabaseClient: MemoSupabaseClient,
   categoryRequest: CategoryTable['Insert'][],
-) => supabaseClient.from('category').upsert(categoryRequest).select();
+) => supabaseClient.from(SUPABASE.schemaCategory).upsert(categoryRequest).select();

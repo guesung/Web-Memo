4c9c451528f2144fdcaa2838b5e015b71d1b0fde
guesung <gueit214@naver.com>
2024-12-05 21:27:54 +0900

    chore: 불필요한 JSON화 제거

--- packages/shared/src/utils/web/Supabase.ts
+++ packages/shared/src/utils/web/Supabase.ts
@@ -3,23 +3,19 @@ import { Database } from '@src/types';
 import { createBrowserClient } from '@supabase/ssr';
 
 export const getSupabaseClient = () =>
-  JSON.parse(
-    JSON.stringify(
-      createBrowserClient<Database>(CONFIG.supabaseUrl, CONFIG.supabaseAnonKey, {
-        auth: {
-          storage: {
-            getItem: key => {
-              return document.cookie.match(new RegExp(`(^| )${key}=([^;]+)`))?.[2] ?? '';
-            },
-            setItem: (key, value) => {
-              document.cookie = `${key}=${value}; path=/; max-age=31536000; SameSite=Strict; Secure`;
-            },
-            removeItem: key => {
-              document.cookie = `${key}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
-            },
-          },
+  createBrowserClient<Database>(CONFIG.supabaseUrl, CONFIG.supabaseAnonKey, {
+    auth: {
+      storage: {
+        getItem: key => {
+          return document.cookie.match(new RegExp(`(^| )${key}=([^;]+)`))?.[2] ?? '';
         },
-        db: { schema: SUPABASE.schemaMemo },
-      }),
-    ),
-  );
+        setItem: (key, value) => {
+          document.cookie = `${key}=${value}; path=/; max-age=31536000; SameSite=Strict; Secure`;
+        },
+        removeItem: key => {
+          document.cookie = `${key}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+        },
+      },
+    },
+    db: { schema: SUPABASE.schemaMemo },
+  });

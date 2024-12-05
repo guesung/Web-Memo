f1d3102850b576fa00a16eae9776f5557afdf662
guesung <gueit214@naver.com>
2024-12-05 12:41:25 +0900

    refactor: `useSupabaseClient`의 불필요한 로직 제거

--- packages/web/src/hooks/useSupabaseClient.ts
+++ packages/web/src/hooks/useSupabaseClient.ts
@@ -1,16 +1,6 @@
-import { MemoSupabaseClient } from '@extension/shared/types';
 import { getSupabaseClient } from '@src/modules/supabase/util.client';
 import { useMemo } from 'react';
 
-let client: MemoSupabaseClient | undefined;
-
-function getSupabaseClientBrowser() {
-  if (client) return client;
-  return getSupabaseClient();
+export default function useSupabaseClient() {
+  return useMemo(getSupabaseClient, []);
 }
-
-function useSupabaseClient() {
-  return useMemo(getSupabaseClientBrowser, []);
-}
-
-export default useSupabaseClient;

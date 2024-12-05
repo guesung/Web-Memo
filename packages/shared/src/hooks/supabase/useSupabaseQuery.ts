bdf2c26e2da548d78ceee27d8946d8dd4a23f530
guesung <gueit214@naver.com>
2024-12-05 16:31:17 +0900

    fix: 클라이언트단에서 패칭하는 Supabase는 `useQuery`로 변경

--- packages/shared/src/hooks/supabase/useSupabaseQuery.ts
+++ packages/shared/src/hooks/supabase/useSupabaseQuery.ts
@@ -2,10 +2,10 @@ import { QUERY_KEY } from '@src/constants';
 import { isExtension } from '@src/utils/Environment';
 import { getSupabaseClient as getSupabaseClientExtension } from '@src/utils/extension';
 import { getSupabaseClient as getSupabaseClientWeb } from '@src/utils/web';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 export default function useSupabaseQuery() {
-  return useSuspenseQuery({
+  return useQuery({
     queryFn: isExtension ? getSupabaseClientExtension : getSupabaseClientWeb,
     queryKey: QUERY_KEY.supabaseClient(),
   });

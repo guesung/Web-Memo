a054f97dc989c3357e9bfe63bfe78dc713483c40
guesung <gueit214@naver.com>
2024-12-05 11:47:33 +0900

    refactor: `private`을  `#`로 교체

--- packages/shared/src/utils/Supabase.ts
+++ packages/shared/src/utils/Supabase.ts
@@ -3,69 +3,81 @@ import { CategoryRow, CategoryTable, MemoRow, MemoSupabaseClient, MemoTable } fr
 import type { QueryData } from '@supabase/supabase-js';
 
 export class MemoService {
-  constructor(private supabaseClient: MemoSupabaseClient) {}
+  #supabaseClient: MemoSupabaseClient;
+
+  constructor(supabaseClient: MemoSupabaseClient) {
+    this.#supabaseClient = supabaseClient;
+  }
 
   async getMemos() {
-    return this.supabaseClient
+    return this.#supabaseClient
       .from(SUPABASE.schemaMemo)
       .select('*,category(name)')
       .order('created_at', { ascending: false });
   }
 
   async insertMemo(memoRequest: MemoTable['Insert']) {
-    return this.supabaseClient.from(SUPABASE.schemaMemo).insert(memoRequest).select();
+    return this.#supabaseClient.from(SUPABASE.schemaMemo).insert(memoRequest).select();
   }
 
   async updateMemo(id: MemoRow['id'], memoRequest: MemoTable['Update']) {
-    return this.supabaseClient.from(SUPABASE.schemaMemo).update(memoRequest).eq('id', id).select();
+    return this.#supabaseClient.from(SUPABASE.schemaMemo).update(memoRequest).eq('id', id).select();
   }
 
   async deleteMemo(id: number) {
-    return this.supabaseClient.from(SUPABASE.schemaMemo).delete().eq('id', id).select();
+    return this.#supabaseClient.from(SUPABASE.schemaMemo).delete().eq('id', id).select();
   }
 
   async deleteMemos(idList: number[]) {
-    return this.supabaseClient.from(SUPABASE.schemaMemo).delete().in('id', idList).select();
+    return this.#supabaseClient.from(SUPABASE.schemaMemo).delete().in('id', idList).select();
   }
 
   async upsertMemos(memoRequest: MemoTable['Insert'][]) {
-    return this.supabaseClient.from(SUPABASE.schemaMemo).upsert(memoRequest).select();
+    return this.#supabaseClient.from(SUPABASE.schemaMemo).upsert(memoRequest).select();
   }
 }
 
 export class CategoryService {
-  constructor(private supabaseClient: MemoSupabaseClient) {}
+  #supabaseClient: MemoSupabaseClient;
+
+  constructor(supabaseClient: MemoSupabaseClient) {
+    this.#supabaseClient = supabaseClient;
+  }
 
   async getCategories() {
-    return this.supabaseClient.from(SUPABASE.schemaCategory).select('*').order('created_at', { ascending: false });
+    return this.#supabaseClient.from(SUPABASE.schemaCategory).select('*').order('created_at', { ascending: false });
   }
 
   async insertCategory(categoryRequest: CategoryTable['Insert']) {
-    return this.supabaseClient.from(SUPABASE.schemaCategory).insert(categoryRequest).select();
+    return this.#supabaseClient.from(SUPABASE.schemaCategory).insert(categoryRequest).select();
   }
 
   async updateCategory(id: CategoryRow['id'], categoryRequest: CategoryTable['Update']) {
-    return this.supabaseClient.from(SUPABASE.schemaCategory).update(categoryRequest).eq('id', id).select();
+    return this.#supabaseClient.from(SUPABASE.schemaCategory).update(categoryRequest).eq('id', id).select();
   }
 
   async deleteCategory(id: CategoryRow['id']) {
-    return this.supabaseClient.from(SUPABASE.schemaCategory).delete().eq('id', id).select();
+    return this.#supabaseClient.from(SUPABASE.schemaCategory).delete().eq('id', id).select();
   }
 
   async upsertCategories(categoryRequest: CategoryTable['Insert'][]) {
-    return this.supabaseClient.from(SUPABASE.schemaCategory).upsert(categoryRequest).select();
+    return this.#supabaseClient.from(SUPABASE.schemaCategory).upsert(categoryRequest).select();
   }
 }
 
 export class AuthService {
-  constructor(private supabaseClient: MemoSupabaseClient) {}
+  #supabaseClient: MemoSupabaseClient;
+
+  constructor(supabaseClient: MemoSupabaseClient) {
+    this.#supabaseClient = supabaseClient;
+  }
 
   getUser() {
-    return this.supabaseClient.auth.getUser();
+    return this.#supabaseClient.auth.getUser();
   }
 
   async checkUserLogin() {
-    const user = await this.supabaseClient.auth.getUser();
+    const user = await this.#supabaseClient.auth.getUser();
     return !!user?.data?.user;
   }
 }

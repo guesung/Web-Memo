b63f68dd8a95a7457431c7fe46031115ddb339d8
guesung <gueit214@naver.com>
2024-12-06 11:42:38 +0900

    fix: 오타 수정

--- packages/shared/src/hooks/supabase/useMemosQuery.ts
+++ packages/shared/src/hooks/supabase/useMemosQuery.ts
@@ -1,21 +1,17 @@
 import { QUERY_KEY } from '@src/constants';
 import { MemoService } from '@src/utils';
-import { getSupabaseClient } from '@src/utils/web';
 import { useSuspenseQuery } from '@tanstack/react-query';
+
+import useSupabaseQuery from './useSupabaseQuery';
 
 type QueryData = Awaited<ReturnType<MemoService['getMemos']>>;
 type QueryError = Error;
 
 export default function useMemosQuery() {
-  // const { data: supabaseClient } = useSupabaseQuery();
-  const supabaseClient = getSupabaseClient();
+  const { data: supabaseClient } = useSupabaseQuery();
 
   const query = useSuspenseQuery<QueryData, QueryError>({
-    queryFn: async () => {
-      const supabase = new MemoService(supabaseClient);
-
-      return supabase.getMemos();
-    },
+    queryFn: () => new MemoService(supabaseClient).getMemos(),
     queryKey: QUERY_KEY.memos(),
   });
 

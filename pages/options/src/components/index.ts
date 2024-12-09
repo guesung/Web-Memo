3acd39017fc29a0d2a1398cfc91cbd59ffb90045
guesung <gueit214@naver.com>
2024-12-09 11:40:33 +0900

    fix: 사용하지 않는 import 제거

--- pages/options/src/components/index.ts
+++ pages/options/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from './Header';
 export { default as Option } from './Option';
-export { default as OptionForm } from './OptionForm';
 export { default as QueryProvider } from './QueryProvider';

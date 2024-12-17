60a4c269ec915604b858a4d6a7bc9708e791f4f0
guesung <gueit214@naver.com>
2024-12-17 21:25:15 +0900

    refactor: 업데이트 상수 분리

---
+++ packages/web/src/constants/Update.ts
@@ -0,0 +1,42 @@
+export const UPDATE_LIST = [
+  {
+    date: '2024.12.18',
+    version: 'v1.7.1',
+  },
+  {
+    date: '2024.12.06',
+    version: 'v1.7.0',
+  },
+  {
+    date: '2024.11.18',
+    version: 'v1.6.0',
+  },
+  {
+    date: '2024.11.15',
+    version: 'v1.5.6',
+  },
+  {
+    date: '2024.10.11',
+    version: 'v1.4.0',
+  },
+  {
+    date: '2024.09.22',
+    version: 'v1.1.0',
+  },
+  {
+    date: '2024.09.21',
+    version: 'v1.0.0',
+  },
+  {
+    date: '2024.09.03',
+    version: 'v0.2.0',
+  },
+  {
+    date: '2024.09.01',
+    version: 'v0.1.0',
+  },
+  {
+    date: '2024.08.28',
+    version: 'v0.0.2',
+  },
+] as const;

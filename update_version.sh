c9499e3d548f8ebeb6066e41e0d1a89c0fe6d3a9
guesung <gueit214@naver.com>
2024-12-18 20:54:47 +0900

    chore: v1.0.0

--- update_version.sh
+++ update_version.sh
@@ -3,6 +3,7 @@
 # FORMAT IS <0.0.0>
 
 if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  # Update version in all package.json files
   find . -name 'package.json' -not -path '*/node_modules/*' -exec bash -c '
     # Parse the version from package.json
     current_version=$(grep -o "\"version\": \"[^\"]*" "$0" | cut -d"\"" -f4)
@@ -11,7 +12,14 @@ if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     perl -i -pe"s/$current_version/'$1'/" "$0"
   '  {} \;
 
-  echo "Updated versions to $1";
+  echo "Updated versions to $1"
+
+  git add .
+  git commit -m "chore: v$1"
+
+  git tag "v$1"
+  git push origin "v$1"
+  git push
 else
-  echo "Version format <$1> isn't correct, proper format is <0.0.0>";
+  echo "Version format <$1> isn't correct, proper format is <0.0.0>"
 fi

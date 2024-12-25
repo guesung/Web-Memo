0b2c6c21b66bf7b1e3dbc69e3d43dfd07daf7f27
guesung <gueit214@naver.com>
2024-12-25 13:32:29 +0900

    feat: 더 구체적인 에러메시지 출력

---
+++ packages/shared/src/modules/extension-bridge/types.ts
@@ -0,0 +1,26 @@
+import { Category } from './constant';
+
+export class ExtensionError extends Error {
+  constructor(
+    message: string,
+    public code: ExtensionErrorCode,
+    public originalError?: unknown,
+  ) {
+    super(message);
+    this.name = 'ExtensionError';
+  }
+}
+
+export enum ExtensionErrorCode {
+  COMMUNICATION_ERROR = 'COMMUNICATION_ERROR',
+  PERMISSION_ERROR = 'PERMISSION_ERROR',
+  CONTENT_ERROR = 'CONTENT_ERROR',
+  RUNTIME_ERROR = 'RUNTIME_ERROR',
+  TAB_ERROR = 'TAB_ERROR',
+  YOUTUBE_ERROR = 'YOUTUBE_ERROR',
+}
+
+export interface PageContent {
+  content: string;
+  category: Category;
+}

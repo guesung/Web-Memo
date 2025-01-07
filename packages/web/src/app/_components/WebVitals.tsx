dc75b4d131601a762d43a56a0d84a9df2d3137c2
guesung <gueit214@naver.com>
2025-01-07 16:48:07 +0900

    feat: CLS를 GA에 전송

--- packages/web/src/app/_components/WebVitals.tsx
+++ packages/web/src/app/_components/WebVitals.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { sendGTMEvent } from '@next/third-parties/google';
 import { useReportWebVitals } from 'next/web-vitals';
 
 export default function WebVitals() {
   useReportWebVitals(metric => {
-    console.log(metric);
+    sendGTMEvent({
+      event: metric.name,
+      eventCategory: 'Web Vitals',
+      eventAction: metric.name,
+      eventLabel: metric.id,
+      value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
+    });
   });
 
   return null;

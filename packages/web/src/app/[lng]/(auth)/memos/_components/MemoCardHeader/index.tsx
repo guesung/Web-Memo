b7e43587975b7539d96504da248b64c233f64c7e
guesung <gueit214@naver.com>
2025-01-08 13:49:15 +0900

    feat: 컴포넌트 및 함수 메모이제이션

--- packages/web/src/app/[lng]/(auth)/memos/_components/MemoCardHeader/index.tsx
+++ packages/web/src/app/[lng]/(auth)/memos/_components/MemoCardHeader/index.tsx
@@ -4,6 +4,7 @@ import { Button, CardHeader, Tooltip, TooltipContent, TooltipProvider, TooltipTr
 import { CheckIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { memo, useCallback } from 'react';
 
 interface MemoCardHeaderProps {
   memo: GetMemoResponse;
@@ -12,10 +13,11 @@ interface MemoCardHeaderProps {
   isSelected?: boolean;
 }
 
-export default function MemoCardHeader({ memo, selectMemoItem, isHovered, isSelected }: MemoCardHeaderProps) {
-  const handleMemoSelect = () => {
+export default memo(function MemoCardHeader({ memo, selectMemoItem, isHovered, isSelected }: MemoCardHeaderProps) {
+  console.log(1);
+  const handleMemoSelect = useCallback(() => {
     selectMemoItem?.(memo.id);
-  };
+  }, []);
 
   const isShowingSelectButton = isHovered || isSelected;
   return (
@@ -55,4 +57,4 @@ export default function MemoCardHeader({ memo, selectMemoItem, isHovered, isSele
       </Link>
     </CardHeader>
   );
-}
+});

281d814cf0f5c5feb09a0be85a65278174af2bf5
guesung <gueit214@naver.com>
2025-01-13 15:39:39 +0900

    refactor: 불필요한 `previousTimeRef` 제거

--- packages/shared/src/hooks/useRAF.ts
+++ packages/shared/src/hooks/useRAF.ts
@@ -2,16 +2,12 @@ import { useCallback, useRef } from 'react';
 
 export const useRAF = () => {
   const frameRef = useRef<number>();
-  const previousTimeRef = useRef<number>();
 
   const schedule = useCallback((callback: () => void) => {
     if (frameRef.current) return;
 
-    const animate = (currentTime: number) => {
-      if (previousTimeRef.current !== undefined) {
-        callback();
-      }
-      previousTimeRef.current = currentTime;
+    const animate = () => {
+      callback();
       frameRef.current = requestAnimationFrame(animate);
     };
 
@@ -22,7 +18,6 @@ export const useRAF = () => {
     if (frameRef.current) {
       cancelAnimationFrame(frameRef.current);
       frameRef.current = undefined;
-      previousTimeRef.current = undefined;
     }
   }, []);
 

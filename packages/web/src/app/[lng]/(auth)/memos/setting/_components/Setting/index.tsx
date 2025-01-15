70a691746b1834266e80f614c629efb44a610d0b
guesung <gueit214@naver.com>
2025-01-15 15:10:08 +0900

    chore: 설정 페이지의 애니메이션 제거

--- packages/web/src/app/[lng]/(auth)/memos/setting/_components/Setting/index.tsx
+++ packages/web/src/app/[lng]/(auth)/memos/setting/_components/Setting/index.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { MOTION_VARIANTS } from '@extension/shared/constants';
 import { Loading } from '@extension/ui';
 import { LanguageType } from '@src/modules/i18n';
-import { motion } from 'framer-motion';
-import { Suspense } from 'react';
 
+import { Suspense } from 'react';
 import SettingCategoryForm from './SettingCategoryForm';
 import SettingGuide from './SettingGuide';
 import SettingLanguage from './SettingLanguage';
@@ -14,17 +12,12 @@ interface SettingProps extends LanguageType {}
 
 export default function Setting({ lng }: SettingProps) {
   return (
-    <motion.section
-      className="grid gap-6"
-      variants={MOTION_VARIANTS.fadeInAndOut}
-      initial="initial"
-      animate="animate"
-      exit="exit">
+    <section className="grid gap-6">
       <Suspense fallback={<Loading />}>
         <SettingLanguage lng={lng} />
         <SettingGuide lng={lng} />
         <SettingCategoryForm lng={lng} />
       </Suspense>
-    </motion.section>
+    </section>
   );
 }

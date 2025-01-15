d95294594660412e6d3d3a3af572c9abfe9784f1
Kuesung Park <gueit214@naver.com>
2025-01-15 16:46:49 +0900

    Feature: 404 Not Found 페이지 (#137)
    * feat: not found 페이지 구현
    
    * feat: lost-astronaunt 아이콘 추가
    
    * refactor: layout 위치를 최상단으로 이동
    
    * fix: language를 넘기지 않을 경우, 기존에 설정된 언어 사용
    
    * feat: i18n 번역
    
    * feat: html - lang 속성 추가
    
    * fix: 오타 수정

---
+++ packages/web/src/app/layout.tsx
@@ -0,0 +1,56 @@
+import '@extension/ui/dist/global.css';
+import './globals.css';
+
+import { GoogleAnalytics } from '@next/third-parties/google';
+import { Toaster } from '@src/components/ui';
+import { CONFIG } from '@src/constants';
+import type { Metadata, Viewport } from 'next';
+import localFont from 'next/font/local';
+import { WebVitals } from './_components';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const pretendard = localFont({
+  src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+  preload: true,
+  fallback: ['-apple-system', 'BlinkMacSystemFont', 'system-ui', 'Roboto', 'sans-serif'],
+});
+
+export const metadata: Metadata = {
+  title: '웹 메모',
+  description: '웹페이지를 쉽게 저장하고 관리하세요',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    title: '웹 메모',
+    description: '웹페이지를 쉽게 저장하고 관리하세요',
+    locale: 'ko_KR',
+    siteName: '웹 메모',
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <html className="h-screen" lang="ko" suppressHydrationWarning>
+      <body className={`${pretendard.variable} font-pretendard h-full`}>
+        {children}
+
+        <WebVitals />
+        <GoogleAnalytics gaId={CONFIG.gaId} />
+        <Toaster />
+      </body>
+    </html>
+  );
+}

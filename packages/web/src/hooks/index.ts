d0c092b26e335fcf6214f1dbdd3e7ed8ae8ea163
guesung <gueit214@naver.com>
2025-01-16 16:32:15 +0900

    feat: 이벤트를 전파하는 `usePropagateEvent` 훅 구현

--- packages/web/src/hooks/index.ts
+++ packages/web/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useGetExtensionManifest } from './useGetExtensionManifest';
 export { default as useLanguage } from './useLanguage';
 export { default as useDrag } from './useDrag';
 export { default as useScrollSync } from './useScrollSync';
+export { default as usePropagateEvent } from './usePropagateEvent';

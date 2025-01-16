18889a9be76a15911f1f363514b972a7ea12dfb4
guesung <gueit214@naver.com>
2025-01-16 12:04:13 +0900

    chore: MemoItem 컴포넌트를 memo의 사용자 정의 비교함수로 메모이제이션

--- packages/web/src/app/[lng]/(auth)/memos/_components/MemoView/MemoItem.tsx
+++ packages/web/src/app/[lng]/(auth)/memos/_components/MemoView/MemoItem.tsx
@@ -16,59 +16,68 @@ interface MemoItemProps extends HTMLAttributes<HTMLElement>, LanguageType {
   isSelected: boolean;
 }
 
-export default memo(function MemoItem({ lng, memo, selectMemoItem, isSelecting, isSelected, ...props }: MemoItemProps) {
-  const searchParams = useSearchParams();
-  const [isHovered, setIsHovered] = useState(false);
-  const [open, setOpen] = useState(memo.id === Number(searchParams.get('id')));
+export default memo(
+  function MemoItem({ lng, memo, selectMemoItem, isSelecting, isSelected, ...props }: MemoItemProps) {
+    const searchParams = useSearchParams();
+    const [isHovered, setIsHovered] = useState(false);
+    const [open, setOpen] = useState(memo.id === Number(searchParams.get('id')));
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
 
-  const handleMemoItemClick = (event: MouseEvent<HTMLElement>) => {
-    const target = event.target as HTMLElement;
-    const isMemoItem = target.closest('.memo-item');
-    if (!isMemoItem) return;
+    const handleMemoItemClick = (event: MouseEvent<HTMLElement>) => {
+      const target = event.target as HTMLElement;
+      const isMemoItem = target.closest('.memo-item');
+      if (!isMemoItem) return;
 
-    const id = event.currentTarget.id;
+      const id = event.currentTarget.id;
 
-    if (isSelecting) selectMemoItem(Number(id));
-    else {
-      setOpen(true);
-      searchParams.set('id', id);
-      window.history.replaceState(
-        { ...window.history.state, as: searchParams.getUrl(), url: searchParams.getUrl() },
-        '',
-        searchParams.getUrl(),
-      );
-    }
-  };
+      if (isSelecting) selectMemoItem(Number(id));
+      else {
+        setOpen(true);
+        searchParams.set('id', id);
+        window.history.replaceState(
+          { ...window.history.state, as: searchParams.getUrl(), url: searchParams.getUrl() },
+          '',
+          searchParams.getUrl(),
+        );
+      }
+    };
 
-  return (
-    <article
-      {...props}
-      id={String(memo.id)}
-      className={cn(
-        'memo-item select-none transition-all [transform:translateZ(0)]',
-        { invisible: open },
-        props.className,
-      )}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleMemoItemClick}>
-      <Card
-        className={cn('relative box-content w-[300px] transition-all', {
-          'border-primary cursor-pointer': isSelected,
-        })}>
-        <MemoCardHeader memo={memo} isHovered={isHovered} isSelected={isSelected} selectMemoItem={selectMemoItem} />
-        {memo.memo && <CardContent className="whitespace-break-spaces break-all">{memo.memo}</CardContent>}
-        <MemoCardFooter memo={memo} lng={lng} isShowingOption={isHovered && !isSelecting} />
-      </Card>
+    return (
+      <article
+        {...props}
+        id={String(memo.id)}
+        className={cn(
+          'memo-item select-none transition-all [transform:translateZ(0)]',
+          { invisible: open },
+          props.className,
+        )}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleMemoItemClick}>
+        <Card
+          className={cn('relative box-content w-[300px] transition-all', {
+            'border-primary cursor-pointer': isSelected,
+          })}>
+          <MemoCardHeader memo={memo} isHovered={isHovered} isSelected={isSelected} selectMemoItem={selectMemoItem} />
+          {memo.memo && <CardContent className="whitespace-break-spaces break-all">{memo.memo}</CardContent>}
+          <MemoCardFooter memo={memo} lng={lng} isShowingOption={isHovered && !isSelecting} />
+        </Card>
 
-      {open && <MemoDialog lng={lng} id={memo.id} open={open} setOpen={setOpen} />}
-    </article>
-  );
-});
+        {open && <MemoDialog lng={lng} id={memo.id} open={open} setOpen={setOpen} />}
+      </article>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.memo.id === nextProps.memo.id &&
+      prevProps.isSelected === nextProps.isSelected &&
+      prevProps.memo.memo === nextProps.memo.memo
+    );
+  },
+);

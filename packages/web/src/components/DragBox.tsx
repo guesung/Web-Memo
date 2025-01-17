1b7b6f248ffde54db8fa35559af3d7206a76b221
guesung <gueit214@naver.com>
2025-01-17 10:29:40 +0900

    design: DragBox의 MemoOptionHeader보다 z-index 낮게 설정

--- packages/web/src/components/DragBox.tsx
+++ packages/web/src/components/DragBox.tsx
@@ -11,7 +11,7 @@ export default forwardRef<HTMLDivElement, DragBoxProps>(function DragBox({ class
   return (
     <div
       className={cn(
-        'bg-primary/20 pointer-events-none fixed left-0 top-0 z-[1000] h-[1px] w-[1px] origin-top-left',
+        'bg-primary/20 pointer-events-none fixed left-0 top-0 z-[40] h-[1px] w-[1px] origin-top-left',
         className,
       )}
       ref={ref}

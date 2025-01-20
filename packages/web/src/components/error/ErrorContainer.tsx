b9202ac8dd9c521fe73c20e22ce9bf53c2e931b2
guesung <gueit214@naver.com>
2025-01-20 14:23:12 +0900

    feat: 500에러 발생 시 sentry에 fatal 레벨로 전송

--- packages/web/src/components/error/ErrorContainer.tsx
+++ packages/web/src/components/error/ErrorContainer.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import * as Sentry from '@sentry/nextjs';
+
 import { Button } from '@extension/ui';
+import { useEffect } from 'react';
 
 interface ErrorContainerProps {
   error: Error & { digest?: string };
@@ -6,14 +11,17 @@ interface ErrorContainerProps {
 }
 
 export default function ErrorContainer({ error, reset }: ErrorContainerProps) {
+  useEffect(() => {
+    Sentry.captureException(error, {
+      level: 'fatal',
+    });
+  }, []);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-4 text-center">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">문제가 발생했습니다</h1>
-      <p className="text-xl text-gray-600 dark:text-gray-300 max-w-lg">{error.message}</p>
-      <Button 
-        onClick={reset} 
-        className="mt-4 px-6 py-3 text-lg shadow-lg hover:shadow-xl transition-all duration-300"
-      >
+      <p className="max-w-lg text-xl text-gray-600 dark:text-gray-300">{error.message}</p>
+      <Button onClick={reset} className="mt-4 px-6 py-3 text-lg shadow-lg transition-all duration-300 hover:shadow-xl">
         다시 시도하기
       </Button>
     </div>

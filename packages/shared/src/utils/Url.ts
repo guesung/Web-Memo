f6e0b02a79723b09fe72d34c96033f863286e09e
Kuesung Park <gueit214@naver.com>
2025-01-23 13:53:19 +0900

    fix: normalizeUrl로 유튜브 별도 처리 (#147)

--- packages/shared/src/utils/Url.ts
+++ packages/shared/src/utils/Url.ts
@@ -1,9 +1,21 @@
-export const formatUrl = (url?: string) => {
-  if (!url) return '';
+const urlNormalizers = {
+  'youtube.com': (url: URL) => {
+    const videoId = url.searchParams.get('v');
+    return `${url.origin}${url.pathname}?v=${videoId}`;
+  },
+};
 
-  const urlWithoutFragment = url.split('#').at(0);
+export const normalizeUrl = (url: string) => {
+  const urlObj = new URL(url);
+  const domain = urlObj.hostname.replace('www.', '');
 
-  if (!urlWithoutFragment) throw new Error('Invalid URL');
+  // 도메인별 특수 처리가 있는 경우 적용
+  for (const [key, normalizer] of Object.entries(urlNormalizers)) {
+    if (domain.includes(key)) {
+      return normalizer(urlObj);
+    }
+  }
 
-  return urlWithoutFragment;
+  // 기본 처리
+  return `${urlObj.origin}${urlObj.pathname}${urlObj.search}`;
 };

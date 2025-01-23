80df3ccba4b817b7b1f88420b39b0a9d1ebfb39f
guesung <gueit214@naver.com>
2025-01-23 16:09:56 +0900

    feat: global-error 구현 및 app/_components로 이동
    ref : https://nextjs.org/docs/14/app/building-your-application/routing/error-handling

--- packages/web/src/app/_components/index.ts
+++ packages/web/src/app/_components/index.ts
@@ -2,3 +2,5 @@ export { default as ExtensionDialog } from './ExtensionDialog';
 export { default as Introduction } from './Introduction';
 export { default as WebVitals } from './WebVitals';
 export { default as InitDayjs } from './InitDayjs';
+export { default as NotFoundContainer } from './NotFoundContainer';
+export { default as ErrorContainer } from './ErrorContainer';

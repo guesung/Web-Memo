b4b7da150ebe38f8967eef28ba6371e72e7e6ab5
guesung <gueit214@naver.com>
2025-01-29 10:51:38 +0900

    chore: type import

--- packages/web/src/app/[lng]/(auth)/memos/page.tsx
+++ packages/web/src/app/[lng]/(auth)/memos/page.tsx
@@ -1,11 +1,11 @@
 'use server';
 
 import { QUERY_KEY } from '@extension/shared/constants';
-import { SearchParamsType } from '@extension/shared/modules/search-params';
+import { type SearchParamsType } from '@extension/shared/modules/search-params';
 import { MemoService } from '@extension/shared/utils';
 import { HydrationBoundaryWrapper } from '@src/components';
 import { Loading } from '@src/components/ui';
-import { LanguageParams } from '@src/modules/i18n';
+import { type LanguageParams } from '@src/modules/i18n';
 import { getSupabaseClient } from '@src/modules/supabase/util.server';
 import { Suspense } from 'react';
 

7d57120172dd971d67f6e5547c7d787806591511
Kuesung Park <gueit214@naver.com>
2025-01-31 22:24:44 +0900

    Fix: 빌드 에러 로그 해결 (#153)
    * fix: 문자열로 수정
    
    * feat: 타입 지원 및 `fallbackLng` 객체 속성 설정
    
    * chore: 빌드 후 소스맵 제거 옵션을 true로 설정
    
    * feat: sentry의 빌드 원격 추정 끄기

--- packages/web/src/modules/i18n/util.server.ts
+++ packages/web/src/modules/i18n/util.server.ts
@@ -5,7 +5,7 @@ import { initReactI18next } from 'react-i18next/initReactI18next';
 import type { Language } from './type';
 import { getOptions } from './util';
 
-const initI18next = async (language: Language, ns?: Namespace) => {
+const initI18next = async (language: Language, namespace?: Namespace) => {
   const i18nInstance = createInstance();
   await i18nInstance
     .use(initReactI18next)
@@ -14,7 +14,7 @@ const initI18next = async (language: Language, ns?: Namespace) => {
         (language: Language, namespace: Namespace) => import(`./locales/${language}/${namespace}.json`),
       ),
     )
-    .init(getOptions(language, ns));
+    .init(getOptions(language, namespace));
   return i18nInstance;
 };
 
@@ -22,11 +22,11 @@ interface TranslationOptions {
   keyPrefix?: string;
 }
 
-export default async function useTranslation(language: Language, ns?: Namespace, options?: TranslationOptions) {
-  const i18nextInstance = await initI18next(language, ns);
+export default async function useTranslation(language: Language, namespace?: Namespace, options?: TranslationOptions) {
+  const i18nextInstance = await initI18next(language, namespace);
 
   return {
-    t: i18nextInstance.getFixedT(language, Array.isArray(ns) ? ns[0] : ns, options?.keyPrefix),
+    t: i18nextInstance.getFixedT(language, Array.isArray(namespace) ? namespace[0] : namespace, options?.keyPrefix),
     i18n: i18nextInstance,
   };
 }

8f617eddba635c77a3a599ce1ab41922b15bb3c9
guesung <gueit214@naver.com>
2025-04-09 10:25:32 +0900

    fix: 메모 - 가이드 테스트 코드

--- tests/e2e/MemosPage.spec.ts
+++ tests/e2e/MemosPage.spec.ts
@@ -1,27 +1,30 @@
+import { REGEXR } from '../lib/constants';
 import { expect, test } from './fixtures';
 
-test.describe('Login Page', () => {
+test.describe('Memos Page', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.getByRole('button', { name: '테스트 계정으로 로그인' }).click();
-    await page.waitForURL(/.*memos/);
+    await page.goto('/en/login', {
+      waitUntil: 'domcontentloaded',
+    });
+    await page.getByRole('button', { name: 'Login with Test' }).click();
+    await page.waitForURL(REGEXR.page.memos);
   });
 
   test.describe('가이드 기능', () => {
     test('메모 페이지 최초 접속시, 가이드를 볼 수 있다.', async ({ page }) => {
       expect(page.locator('#driver-popover-description')).toHaveText(
-        '메모를 한 번 해볼까요?\nOption + S를 눌러 사이드 패널을 열어보세요 !',
+        "Ready to start? Press 'Option + S' to open the side panel",
       );
     });
     test('사이드 패널을 열 시, 다음 가이드 페이지로 이동한다.', async ({ page, baseURL }) => {
       await page.goto('https://blog.toss.im/article/toss-team-culture');
-      await page.locator('#open-side-panel').click();
+      await page.locator('#OPEN_SIDE_PANEL_BUTTON').click();
       await page.goto(baseURL!);
 
       await page.waitForTimeout(1000);
 
-      expect(page.locator('#driver-popover-description')).toContainText(
-        '이제 이 사이드 패널에서 메모를 기록하실 수 있답니다.',
+      expect(page.locator('#driver-popover-description')).toHaveText(
+        "Great! Now you can write memos. Don't worry, they save automatically",
       );
     });
     test('새로고침 버튼을 누르면, 가이드가 종료된다.', async ({ page }) => {
@@ -40,8 +43,8 @@ test.describe('Login Page', () => {
 
       await page.goto(baseURL!);
     });
+    const text = String(new Date());
     test('사이드 패널에서 메모를 저장하면 메모를 확인할 수 있다.', async ({ page, sidePanelPage }) => {
-      const text = String(new Date());
       await sidePanelPage.locator('#memo-textarea').fill(text);
       await sidePanelPage.locator('#memo-textarea').press('ControlOrMeta+s');
 

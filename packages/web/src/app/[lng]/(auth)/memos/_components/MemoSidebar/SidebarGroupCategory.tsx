25f942c8aeadb2c82d031c8a5c9987c6ea41df62
guesung <gueit214@naver.com>
2025-04-11 12:25:36 +0900

    refactor: 카테고리를 Link태그로 수정

--- packages/web/src/app/[lng]/(auth)/memos/_components/MemoSidebar/SidebarGroupCategory.tsx
+++ packages/web/src/app/[lng]/(auth)/memos/_components/MemoSidebar/SidebarGroupCategory.tsx
@@ -12,7 +12,7 @@ import {
 } from '@src/components/ui';
 import type { LanguageType } from '@src/modules/i18n';
 import useTranslation from '@src/modules/i18n/util.client';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { memo } from 'react';
 
 import SidebarMenuItemAddCategory from './SidebarMenuItemAddCategory';
@@ -20,16 +20,8 @@ import SidebarMenuItemAddCategory from './SidebarMenuItemAddCategory';
 export default memo(function SidebarGroupCategory({ lng }: LanguageType) {
   const { t } = useTranslation(lng);
   const { categories } = useCategoryQuery();
-  const router = useRouter();
   const searchParams = useSearchParams();
-
   const currentCategory = searchParams.get('category');
-
-  const handleCategoryClick = (category: string) => {
-    searchParams.removeAll('isWish');
-    searchParams.set('category', category);
-    router.push(searchParams.getUrl(), { scroll: true });
-  };
 
   return (
     <SidebarGroup>
@@ -38,15 +30,11 @@ export default memo(function SidebarGroupCategory({ lng }: LanguageType) {
         <SidebarMenu>
           {categories?.map(category => (
             <SidebarMenuItem key={category.id}>
-              <SidebarMenuButton asChild>
-                <button
-                  onClick={() => handleCategoryClick(category.name)}
-                  className={`hover:bg-accent flex w-full items-center justify-between rounded-md p-2 ${
-                    currentCategory === category.name && 'bg-accent'
-                  }`}>
-                  <span>{category.name}</span>
-                </button>
-              </SidebarMenuButton>
+              <Link href={`/memos?category=${encodeURIComponent(category.name)}`}>
+                <SidebarMenuButton>
+                  <span className={`${currentCategory === category.name ? 'font-bold' : ''}`}>{category.name}</span>
+                </SidebarMenuButton>
+              </Link>
             </SidebarMenuItem>
           ))}
           <SidebarMenuItemAddCategory />

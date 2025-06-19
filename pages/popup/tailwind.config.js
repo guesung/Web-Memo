f0584110c0e0090752ddb415952d68b88fee7df6
guesung <gueit214@naver.com>
2025-06-19 17:13:32 +0900

    refactor:미사용 패키지 제거

--- pages/popup/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import baseConfig from '@extension/tailwindcss-config';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  ...baseConfig,
-  content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
-};

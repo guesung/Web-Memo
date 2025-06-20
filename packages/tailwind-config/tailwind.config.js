722e8a62bb7e9f974716b4c770f33cbdcbcbe10e
guesung <gueit214@naver.com>
2025-06-20 10:59:38 +0900

    fix: tailwind config를 export 방식으로 수정

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
   theme: {
     extend: {

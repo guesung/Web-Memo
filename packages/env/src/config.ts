d71c907568614bfa7b58aae5e13dc38e7d4caec5
guesung <gueit214@naver.com>
2025-06-22 13:40:58 +0900

    feat: 환경 변수를 관리하는 env 패키지

---
+++ packages/env/src/config.ts
@@ -0,0 +1,33 @@
+
+export const getSafeConfig = (
+	name: string,
+	value: string | undefined,
+): string => {
+	if (value === undefined) throw new Error(`${name}이 설정되지 않았습니다.`);
+	else return value;
+};
+
+
+
+export const CONFIG = {
+	webUrl: getSafeConfig("WEB_URL", process.env.WEB_URL),
+	supabaseUrl: getSafeConfig("SUPABASE_URL", process.env.SUPABASE_URL),
+	supabaseAnonKey: getSafeConfig(
+		"SUPABASE_ANON_KEY",
+		process.env.SUPABASE_ANON_KEY,
+	),
+	sentryDsnExtension: getSafeConfig("SENTRY_DSN", process.env.SENTRY_DSN),
+	sentryDsnWeb: getSafeConfig("SENTRY_DSN_WEB", process.env.SENTRY_DSN_WEB),
+	sentryAuthToken: getSafeConfig(
+		"SENTRY_AUTH_TOKEN",
+		process.env.SENTRY_AUTH_TOKEN,
+	),
+	nodeEnv: getSafeConfig("NODE_ENV", process.env.NODE_ENV) as
+		| "development"
+		| "production",
+	makeWebhookNotionApi: getSafeConfig(
+		"MAKE_WEBHOOK_NOTION_API",
+		process.env.MAKE_WEBHOOK_NOTION_API,
+	) as "development" | "production",
+	openApiKey: getSafeConfig("OPENAI_API_KEY", process.env.OPENAI_API_KEY),
+};
---
+++ packages/env/tsup.config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  clean: true,
+  env: dotenv.config().parsed,
+
+})

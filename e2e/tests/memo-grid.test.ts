04fc54eb1150736b580bb6b2a31ae1096607b7dd
guesung <gueit214@naver.com>
2025-06-27 12:09:36 +0900

    test: 메모 그리드 테스트 코드

---
+++ e2e/tests/memo-grid.test.ts
@@ -0,0 +1,35 @@
+import { PATHS } from "@web-memo/shared/constants";
+import { expect, test } from "./fixtures";
+import {
+	fillMemo,
+	findSidePanelPage,
+	LANGUAGE,
+	login,
+	openSidePanel,
+	skipGuide,
+} from "./lib";
+
+test.describe("메모 그리드", () => {
+	let memoText: string;
+	test.beforeAll(async ({ page }) => {
+		// 메모를 생성한다.
+		await login(page);
+		await openSidePanel(page);
+		const sidePanelPage = await findSidePanelPage(page);
+		memoText = String(new Date());
+		await fillMemo(sidePanelPage, memoText);
+	});
+	test.beforeEach(async ({ page }) => {
+		// 메모 페이지에 접속한다.
+		await login(page);
+		await page.goto(`${LANGUAGE}${PATHS.memos}`);
+	});
+	test("메모 페이지에 접속하면, 메모를 확인할 수 있다.", async ({ page }) => {
+		await expect(page.getByText(memoText)).toBeVisible();
+	});
+	test("메모를 누르면, 메모 상세 페이지가 열린다.", async ({ page }) => {
+		await skipGuide(page);
+		await page.getByText(memoText).click();
+		await expect(page.getByTestId("memo-textarea")).toHaveValue(memoText);
+	});
+});

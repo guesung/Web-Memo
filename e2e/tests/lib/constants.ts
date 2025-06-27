4f12bc52d314865b2dc46657f31748dc0f2ecf9a
guesung <gueit214@naver.com>
2025-06-27 11:17:09 +0900

    refactor: url 상수화

--- e2e/tests/lib/constants.ts
+++ e2e/tests/lib/constants.ts
@@ -1,1 +1,2 @@
 export const LANGUAGE = "en";
+export const TEST_BLOG_URL = "https://blog.toss.im/article/toss-team-culture";
--- e2e/tests/lib/utils.ts
+++ e2e/tests/lib/utils.ts
@@ -1,24 +1,25 @@
 import type { Page } from "@playwright/test";
 import { PATHS } from "@web-memo/shared/constants";
 import { expect } from "../fixtures";
-import { LANGUAGE } from "./constants";
+import { LANGUAGE, TEST_BLOG_URL } from "./constants";
 
 export async function fillMemo(page: Page, text: string) {
-	page.locator("#memo-textarea").fill(text);
+	await page.locator("#memo-textarea").fill(text);
 	await expect(page.locator("#memo-textarea")).toHaveValue(text);
 }
 
 export async function openSidePanel(page: Page) {
-	await page.goto("https://blog.toss.im/article/toss-team-culture");
+	await page.goto(TEST_BLOG_URL);
 	await page.locator("#OPEN_SIDE_PANEL_BUTTON").click();
 	await page.waitForTimeout(1000);
 }
 
 export async function login(page: Page) {
-	await page.goto(`${LANGUAGE}${PATHS.login}`);
+	await page.goto(`/${LANGUAGE}${PATHS.login}`);
+	await page.waitForURL(new RegExp(PATHS.login));
+
 	await page.getByTestId("test-login-button").click();
 	await page.waitForURL(new RegExp(PATHS.memos));
-	await page.waitForTimeout(1000);
 }
 
 export async function findSidePanelPage(page: Page) {

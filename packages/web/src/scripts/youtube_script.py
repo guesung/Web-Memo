3280ae19eff62eadfbd234a97ca548dc1690ebb0
guesung <gueit214@naver.com>
2025-08-10 17:32:28 +0900

    refactor: 불필요 함수 제거

--- packages/web/src/scripts/youtube_script.py
+++ packages/web/src/scripts/youtube_script.py
@@ -2,21 +2,16 @@ import argparse
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
-def get_video_id(video_url):
-    video_id = video_url.split('v=')[1][:11]
-    return video_id
-
 def main():
     # 명령행 인자 파싱
     parser = argparse.ArgumentParser(description='YouTube 동영상 자막을 다운로드합니다.')
     parser.add_argument('--video-id', required=True, help='YouTube 동영상 ID')
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     video_id = args.video_id
 
-    ytt_api = YouTubeTranscriptApi()
-
     try:
+        ytt_api = YouTubeTranscriptApi()
         transcript_list = ytt_api.list(video_id)
         transcript = transcript_list.find_transcript(['ko', 'en'])
         transcript_data = transcript.fetch()
@@ -29,8 +24,6 @@ def main():
     text_formatted = text_formatter.format_transcript(transcript_data)
 
     print(text_formatted)
-
-
 
 if __name__ == "__main__":
     main()

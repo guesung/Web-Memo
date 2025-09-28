3d55726427d3a62f18501289bf2ec7fa9786e919
guesung <gueit214@naver.com>
2025-09-28 13:17:06 +0900

    design: 사이드 패널 여는 버튼을 투명하게 설정

--- pages/content-ui/src/components/OpenSidePanelButton.tsx
+++ pages/content-ui/src/components/OpenSidePanelButton.tsx
@@ -3,12 +3,10 @@ import { ExtensionBridge } from "@web-memo/shared/modules/extension-bridge";
 export default function OpenSidePanelButton() {
 	return (
 		<button
-			className="fixed bottom-4 right-4 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-lg hover:bg-gray-50"
+			className="fixed bottom-4 left-4 z-50 flex h-1 w-1 items-center justify-center rounded-full bg-transparent shadow-lg "
 			type="button"
 			id="OPEN_SIDE_PANEL_BUTTON"
 			onClick={ExtensionBridge.requestOpenSidePanel}
-		>
-			<img alt="logo" className="h-6 w-6" src="/icons/icon-48.png" />
-		</button>
+		/>
 	);
 }

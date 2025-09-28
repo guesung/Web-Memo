76db8298ee852013d5768e3942344629155d651d
guesung <gueit214@naver.com>
2025-09-28 13:41:08 +0900

    chore: 웹에서 로그아웃 시, 익스텐션에서도 즉시 로그아웃

--- pages/side-panel/src/hoc/withAuthentication.tsx
+++ pages/side-panel/src/hoc/withAuthentication.tsx
@@ -1,11 +1,26 @@
 import { LoginSection } from "@src/components";
-import { useSupabaseUserQuery } from "@web-memo/shared/hooks";
+import { SUPABASE } from "@web-memo/shared/constants";
+import { useDidMount, useSupabaseUserQuery } from "@web-memo/shared/hooks";
+import { ExtensionBridge } from "@web-memo/shared/modules/extension-bridge";
+import { toast } from "@web-memo/ui";
 
 export default function withAuthentication(
 	WrappedComponent: () => JSX.Element,
 ) {
 	const AuthenticatedComponent = () => {
-		const { user } = useSupabaseUserQuery();
+		const { user, refetch } = useSupabaseUserQuery();
+
+		useDidMount(() => {
+			ExtensionBridge.responseSignout(async () => {
+				toast({
+					title: "웹에서 로그아웃 되었습니다.",
+					description: " 확장 프로그램에서도 로그아웃 됩니다.",
+				});
+
+				refetch();
+				chrome.storage.sync.remove(SUPABASE.authToken);
+			});
+		});
 
 		if (user?.data.user) return <WrappedComponent />;
 		return <LoginSection />;

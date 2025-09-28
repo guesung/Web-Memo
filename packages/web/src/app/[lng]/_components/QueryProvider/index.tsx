ff609c4e36c1b1ffaa64f95dc4bb75f6a187af51
guesung <gueit214@naver.com>
2025-09-28 12:31:00 +0900

    chore: 불필요한 토스트 제거

--- packages/web/src/app/[lng]/_components/QueryProvider/index.tsx
+++ packages/web/src/app/[lng]/_components/QueryProvider/index.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { captureException } from "@sentry/nextjs";
 import type { LanguageType } from "@src/modules/i18n";
-import useTranslation from "@src/modules/i18n/util.client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ExtensionBridge } from "@web-memo/shared/modules/extension-bridge";
-import { toast } from "@web-memo/ui";
 import type { PropsWithChildren } from "react";
 import { useState } from "react";
 
 interface QueryProviderProps extends PropsWithChildren, LanguageType {}
 
-export default function QueryProvider({ children, lng }: QueryProviderProps) {
-	const { t } = useTranslation(lng);
-
+export default function QueryProvider({ children }: QueryProviderProps) {
 	const [queryClient] = useState(
 		() =>
 			new QueryClient({
@@ -21,12 +16,6 @@ export default function QueryProvider({ children, lng }: QueryProviderProps) {
 					mutations: {
 						onSuccess: async () => {
 							await ExtensionBridge.requestRefetchTheMemos();
-						},
-						onError: (error) => {
-							toast({ title: t("toastTitle.errorSave") });
-							captureException(error, {
-								level: "fatal",
-							});
 						},
 					},
 				},

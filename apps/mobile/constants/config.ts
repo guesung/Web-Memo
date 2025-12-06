bb889961ddc906b8a30bf4cf10212347f28131d7
guesung <gueit214@naver.com>
2025-12-06 10:27:43 +0900

    chore: 모바일 관련 변경사항 제거

--- apps/mobile/constants/config.ts
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
-export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
-export const WEBAPP_URL =
-	process.env.EXPO_PUBLIC_WEBAPP_URL ?? "https://web-memos.vercel.app";

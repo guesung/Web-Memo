4e071a82c2f3c736c316de1122274bc991395216
guesung <gueit214@naver.com>
2026-02-16 09:36:30 +0900

    fix: MemoDialog 내 드롭다운 닫을 때 페이지 프리즈 수정
    DialogContent의 수동 이벤트 핸들러(overlay onClick, useKeyboardBind)를
    Radix의 내장 onInteractOutside/onEscapeKeyDown으로 교체하여
    중첩된 DropdownMenu와 Dialog 간의 dismiss 조율 문제 해결

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { useKeyboardBind } from "@web-memo/shared/hooks";
 import { X } from "lucide-react";
 import * as React from "react";
 
@@ -39,13 +38,20 @@ const DialogContent = React.forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Content>,
 	DialogContentProps
 >(({ className, children, onClose, ...props }, ref) => {
-	useKeyboardBind({ key: "Escape", callback: onClose ?? (() => {}) });
-
 	return (
 		<DialogPortal>
-			<DialogOverlay onClick={onClose} />
+			<DialogOverlay />
 			<DialogPrimitive.Content
 				ref={ref}
+				onInteractOutside={() => onClose?.()}
+				onEscapeKeyDown={(event) => {
+					const target = event.target;
+					if (target instanceof HTMLElement) {
+						const tagName = target.tagName.toLowerCase();
+						if (tagName === "input" || tagName === "textarea" || target.isContentEditable) return;
+					}
+					onClose?.();
+				}}
 				className={cn(
 					"bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg",
 					className,

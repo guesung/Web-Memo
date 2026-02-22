67470cfc1face8820bc3debc0b5fe98cf1c2fff3
guesung <gueit214@naver.com>
2026-02-22 11:25:02 +0900

    chore: Metro resolveRequest 워크어라운드 제거
    React 19로 모든 패키지가 통일되어 React 18/19 버전 충돌 해결용
    resolveRequest 커스텀 리졸버가 불필요해졌으므로 제거

--- apps/app/metro.config.js
+++ apps/app/metro.config.js
@@ -6,27 +6,11 @@ const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// 모노레포 루트의 node_modules도 탐색하도록 설정
 config.watchFolders = [monorepoRoot];
 
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
-
-// react / react-dom / react-native는 앱의 node_modules에서만 resolve하여 단일 인스턴스 보장
-const singleInstancePackages = ['react', 'react-dom', 'react-native'];
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  for (const pkg of singleInstancePackages) {
-    if (moduleName === pkg || moduleName.startsWith(pkg + '/')) {
-      return {
-        filePath: require.resolve(moduleName, { paths: [projectRoot] }),
-        type: 'sourceFile',
-      };
-    }
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;

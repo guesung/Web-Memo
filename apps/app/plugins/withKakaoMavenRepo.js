431e4270e1e3d54c9c6a511fb220da68a710842d
guesung <gueit214@naver.com>
2026-02-23 16:08:40 +0900

    fix: Android 빌드 에러 수정 (Kotlin 버전, Kakao Maven, Gradle 메모리)
    - @react-native-seoul/kakao-login 플러그인에 kotlinVersion 2.0.21 전달하여 KSP 호환성 에러 해결
    - expo-build-properties 플러그인 추가하여 Kotlin 버전 명시적 설정
    - 커스텀 Expo config 플러그인으로 Kakao SDK Maven 저장소 추가
    - Gradle JVM 메모리 증가 (Xmx 4096m, MaxMetaspaceSize 1024m)로 EAS 빌드 OOM 해결
    - .gitignore에 .aab 파일 추가

---
+++ apps/app/plugins/withKakaoMavenRepo.js
@@ -0,0 +1,17 @@
+const { withProjectBuildGradle } = require("@expo/config-plugins");
+
+const KAKAO_MAVEN_REPO = "maven { url 'https://devrepo.kakao.com/nexus/content/groups/public/' }";
+
+function withKakaoMavenRepo(config) {
+  return withProjectBuildGradle(config, (config) => {
+    if (!config.modResults.contents.includes("devrepo.kakao.com")) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /allprojects\s*\{\s*\n\s*repositories\s*\{/,
+        `allprojects {\n  repositories {\n    ${KAKAO_MAVEN_REPO}`
+      );
+    }
+    return config;
+  });
+}
+
+module.exports = withKakaoMavenRepo;

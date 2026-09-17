186c2b1eec0c93752a017698f0f5f812de82690e
frontend-guesung <gueit214@naver.com>
2026-09-18 06:59:46 +0900

    [FE] apps/web/src/components/index.ts에 ChannelTalk export 추가

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as ChannelTalk } from "./ChannelTalk";
 export { default as DragBox } from "./DragBox";
 export { default as HydrationBoundaryWrapper } from "./HydrationBoundaryWrapper";
 export { default as YoutubeEmbed } from "./YoutubeEmbed";

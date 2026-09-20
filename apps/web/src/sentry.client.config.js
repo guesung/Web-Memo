f4cb438561a8066b565343006b8e94f0f0ff40d6
guesung <gueit214@naver.com>
2026-09-20 11:17:03 +0900

    chore: 확장과 웹의 Sentry replay를 끈다
    replay 할당량(Developer 플랜 월 50개)이 이미 소진돼, 오류가 날 때마다 나가는
    replay 전송이 429로 거절되고 콘솔에 quota 문구가 뜬다. replaysOnErrorSampleRate가
    1.0이라 오류 하나마다 replay가 함께 나가 할당량이 금방 찬다.
    
    확장 페이지와 웹에서 화면 녹화의 효용이 작아 replayIntegration과 샘플링 설정을
    제거하고, replay 전용이던 사이드 패널의 @sentry-vendor 청크 규칙도 지운다.

--- apps/web/src/sentry.client.config.js
+++ apps/web/src/sentry.client.config.js
@@ -1,10 +1,7 @@
-import { init, replayIntegration } from "@sentry/nextjs";
+import { init } from "@sentry/nextjs";
 import { SENTRY } from "@web-memo/shared/constants";
 
 init({
 	dsn: SENTRY.dsnWeb,
-	integrations: [replayIntegration()],
 	tracesSampleRate: 1.0,
-	replaysSessionSampleRate: 0.1,
-	replaysOnErrorSampleRate: 1.0,
 });
